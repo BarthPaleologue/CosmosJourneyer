28bf898749e18d5572602b04402d410d7ccc4bda
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2022-04-12 19:08:11 +0200

    Updated test config

--- jest.config.ts
+++ jest.config.ts
@@ -81,7 +81,7 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -172,13 +172,16 @@ export default {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
-  // ],
+  transformIgnorePatterns: [
+     "/node_modules/(?!@babylonjs)",
+     "\\.pnp\\.[^\\/]+$"
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
--- src/ts/tests/planet.test.ts
+++ src/ts/tests/planet.test.ts
@@ -1,5 +1,21 @@
+import {Engine, Scene, Vector3} from "@babylonjs/core";
+
+import { JSDOM } from "jsdom"
+const dom = new JSDOM();
+global.document = dom.window.document;
+//@ts-ignore
+global.window = dom.window;
+global.navigator = dom.window.navigator
+
+//import {SolidPlanet} from "../components/celestialBodies/planets/solid/solidPlanet";
+
+let canvas = dom.window.document.createElement("canvas");
+let engine = new Engine(canvas);
+let scene = new Scene(engine);
+
 describe("planet", () => {
-    it('exists', () => {
-        expect(true).toBe(true);
+    //let planet = new SolidPlanet("planet", 10, Vector3.Zero(), 1, scene);
+    it('is correctly initialized', () => {
+        expect(Vector3.Zero()).toBe(Vector3.Zero());
     });
 });
