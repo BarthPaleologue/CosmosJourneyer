5416d55746d8b9e160f9ed161872bc5c8a526826
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2022-07-28 19:10:04 +0200

    Delete tanhSharpener.glsl

--- src/shaders/utils/tanhSharpener.glsl
+++
@@ -1,9 +0,0 @@
-float tanh01(float x) {
-    return (tanh(x) + 1.0) / 2.0;
-}
-float tanhSharpener(float x, float s) {
-    float sampleValue = (x - 0.5) * s;
-    return tanh01(sampleValue);
-}
-
-#pragma glslify: export(tanhSharpener)
