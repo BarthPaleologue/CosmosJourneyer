204daa542a18513580e5129752ae4eb387672d75
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2022-10-06 16:02:39 +0200

    better declaration of webgpu engine

--- src/ts/utils/init.ts
+++ src/ts/utils/init.ts
@@ -1,18 +1,18 @@
-import { Engine, WebGPUEngine } from "@babylonjs/core";
+import { Engine, EngineFactory, WebGPUEngine } from "@babylonjs/core";
 import { UberScene } from "../core/uberScene";
 
 export async function initEngineScene(canvas: HTMLCanvasElement): Promise<[Engine, UberScene]> {
-    const engine = !("gpu" in navigator) ? new Engine(canvas, false, {
+    const engine = await EngineFactory.CreateAsync(canvas, {
+        antialias: false
         //useHighPrecisionFloats: true,
         //useHighPrecisionMatrix: true,
         //adaptToDeviceRatio: true,
-    }) : new WebGPUEngine(canvas);
-
-    if("gpu" in navigator) await (engine as WebGPUEngine).initAsync();
+    });
 
     engine.loadingScreen.displayLoadingUI();
 
-    console.log("GPU utilisé : " + engine.getGlInfo().renderer);
+    console.log(`API: ${engine instanceof WebGPUEngine ? "WebGPU" : "WebGL" + engine.webGLVersion}`);
+    console.log(`GPU detected: ${engine.getGlInfo().renderer}`);
 
     const scene = new UberScene(engine);
 
