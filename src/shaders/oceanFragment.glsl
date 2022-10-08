8eeb2732af2e93f7118a474fcdfb761b76f96c2f
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2022-10-08 18:44:57 +0200

    fixed broken ocean shader

--- src/shaders/oceanFragment.glsl
+++ src/shaders/oceanFragment.glsl
@@ -9,8 +9,8 @@ uniform sampler2D normalMap2;
 
 uniform vec3 cameraPosition; // position of the camera in world space
 
-uniform mat4 projection; // camera's projection matrix
-uniform mat4 view; // camera's view matrix
+uniform mat4 inverseProjection; // camera's projection matrix
+uniform mat4 inverseView; // camera's view matrix
 
 uniform float cameraNear; // camera minZ
 uniform float cameraFar; // camera maxZ
@@ -34,7 +34,7 @@ uniform float time;
 
 #pragma glslify: remap = require(./utils/remap.glsl)
 
-#pragma glslify: worldFromUV = require(./utils/worldFromUV.glsl, projection=projection, view=view)
+#pragma glslify: worldFromUV = require(./utils/worldFromUV.glsl, inverseProjection=inverseProjection, inverseView=inverseView)
 
 #pragma glslify: rayIntersectSphere = require(./utils/rayIntersectSphere.glsl)
 
