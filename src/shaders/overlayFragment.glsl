27f72fae5298bf8b9d7fe4d2bde9d52eff7f389d
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2022-10-14 14:59:25 +0200

    Update overlayFragment.glsl

--- src/shaders/overlayFragment.glsl
+++ src/shaders/overlayFragment.glsl
@@ -1,9 +1,5 @@
 precision lowp float;
 
-#define MAX_PLANETS 10
-uniform vec4 planetPositions[MAX_PLANETS];
-uniform int nbPlanets; // number of stars
-
 uniform sampler2D textureSampler;
 uniform sampler2D depthSampler;
 
@@ -16,6 +12,9 @@ uniform float cameraNear;
 uniform float cameraFar;
 uniform vec3 cameraPosition;
 uniform vec3 cameraDirection;
+
+uniform vec3 planetPosition;
+uniform float planetRadius;
 
 uniform bool isEnabled;
 
@@ -48,37 +47,30 @@ void main() {
 
     vec3 overlayColor = vec3(0.0);
 
-    for(int i = 0; i < nbPlanets; i++) {
-        vec3 planetPosition = planetPositions[i].xyz;
+    float planetDistance = length(planetPosition - cameraPosition);
+    vec3 planetDirection = (planetPosition - cameraPosition) / planetDistance;
 
-        float planetRadius = planetPositions[i].w * 1.5;
-        float planetDistance = length(planetPosition - cameraPosition);
-        vec3 planetDirection = (planetPosition - cameraPosition) / planetDistance;
+    vec2 uv = uvFromWorld(planetPosition);
 
-        if(dot(planetDirection, cameraDirection) < 0.0) continue;
-
-        vec2 uv = uvFromWorld(planetPosition);
-
-        vec2 vUVSquare = vec2(vUV.x * aspectRatio, vUV.y);
-        vec2 uvSquare = vec2(uv.x * aspectRatio, uv.y);
-        float distance = length(uvSquare - vUVSquare);
-        vec2 unitUVSquare = (uvSquare - vUVSquare) / distance;
-        vec3 color = vec3(0.0);
-        float limit1 = 0.03 * pow(planetRadius / 1e6, 0.2);
-        float limit2 = 0.032 * pow(planetRadius / 1e6, 0.2);
-        if(distance >= limit1 && distance <= limit2) {
-            float angle = atan(unitUVSquare.y, unitUVSquare.x);
-            float angleOff = 0.2;
-            if((angle > angleOff && angle < 0.5 * 3.14 - angleOff)
-            || (angle < -angleOff && angle > -0.5 * 3.14 + angleOff)
-            || (angle > 0.5 * 3.14 + angleOff && angle < 3.14 - angleOff)
-            || (angle < -0.5 * 3.14 - angleOff && angle > -3.14 + angleOff)) {
-                color = 0.5e-3 * vec3(planetDistance / planetRadius);
-                color = min(color, vec3(0.3));
-            }
+    vec2 vUVSquare = vec2(vUV.x * aspectRatio, vUV.y);
+    vec2 uvSquare = vec2(uv.x * aspectRatio, uv.y);
+    float distance = length(uvSquare - vUVSquare);
+    vec2 unitUVSquare = (uvSquare - vUVSquare) / distance;
+    vec3 color = vec3(0.0);
+    float limit1 = 0.03 * pow(planetRadius / 1e6, 0.2);
+    float limit2 = 0.032 * pow(planetRadius / 1e6, 0.2);
+    if(distance >= limit1 && distance <= limit2) {
+        float angle = atan(unitUVSquare.y, unitUVSquare.x);
+        float angleOff = 0.2;
+        if((angle > angleOff && angle < 0.5 * 3.14 - angleOff)
+        || (angle < -angleOff && angle > -0.5 * 3.14 + angleOff)
+        || (angle > 0.5 * 3.14 + angleOff && angle < 3.14 - angleOff)
+        || (angle < -0.5 * 3.14 - angleOff && angle > -3.14 + angleOff)) {
+            color = 0.5e-3 * vec3(planetDistance / planetRadius);
+            color = min(color, vec3(0.3));
         }
-        overlayColor = max(overlayColor, color);
     }
+    overlayColor = max(overlayColor, color);
 
     gl_FragColor = vec4(screenColor + overlayColor, 1.0);
 }
