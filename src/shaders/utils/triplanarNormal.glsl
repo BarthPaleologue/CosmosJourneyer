ec2fee99be351d6b9a58dff1193f5ecde194ac60
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-03-08 20:02:42 +0100

    added basis for stochastic texture sampling

--- src/shaders/utils/triplanarNormal.glsl
+++ src/shaders/utils/triplanarNormal.glsl
@@ -1,8 +1,35 @@
+float rand(vec2 co) {
+    return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+vec2 stochasticSample(vec2 uv, float seed) {
+    float jitter = rand(uv + seed) * 0.01;
+    return uv + vec2(jitter, jitter);
+}
+
 // https://bgolus.medium.com/normal-mapping-for-a-triplanar-shader-10bf39dca05a
 vec3 triplanarNormal(vec3 position, vec3 surfaceNormal, sampler2D normalMap, float scale, float sharpness, float normalStrength) {
     vec2 uvX = position.zy * scale;
     vec2 uvY = position.xz * scale;
     vec2 uvZ = position.xy * scale;
+
+    // each uv is the result of a blending between 3 stochastic samples
+    /*vec2 uvX = vec2(0.0);
+    vec2 uvY = vec2(0.0);
+    vec2 uvZ = vec2(0.0);
+
+    for (int i = 0; i < 3; i++) {
+        float seed = float(i) * 0.1;
+        uvX += stochasticSample(position.zy * scale, seed);
+        uvY += stochasticSample(position.xz * scale, seed);
+        uvZ += stochasticSample(position.xy * scale, seed);
+    }
+
+    uvX /= 3.0;
+    uvY /= 3.0;
+    uvZ /= 3.0;*/
+
+    // get the normal from the normal map
 
     vec3 tNormalX = texture2D(normalMap, uvX).rgb;
     vec3 tNormalY = texture2D(normalMap, uvY).rgb;
