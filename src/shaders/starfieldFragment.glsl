577ff93785197fad572fbf6ed10c016a54a387fc
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-03-18 17:01:52 +0100

    starfield now works with plume

--- src/shaders/starfieldFragment.glsl
+++ src/shaders/starfieldFragment.glsl
@@ -12,12 +12,7 @@ uniform vec3 cameraPosition; // position of the camera in world space
 uniform mat4 inverseProjection; // camera's projection matrix
 uniform mat4 inverseView; // camera's view matrix
 
-uniform float cameraNear; // camera minZ
-uniform float cameraFar; // camera maxZ
-
 uniform float visibility; // visibility of the starfield
-
-#pragma glslify: remap = require(./utils/remap.glsl)
 
 #pragma glslify: worldFromUV = require(./utils/worldFromUV.glsl, inverseProjection=inverseProjection, inverseView=inverseView)
 
@@ -30,14 +25,9 @@ void main() {
 
     vec3 rayDir = normalize(pixelWorldPosition - cameraPosition); // normalized direction of the ray
 
-    // closest physical point from the camera in the direction of the pixel (occlusion)
-    vec3 closestPoint = (pixelWorldPosition - cameraPosition) * remap(depth, 0.0, 1.0, cameraNear, cameraFar);
-    float maximumDistance = length(closestPoint); // the maxium ray length due to occlusion
-
     vec4 finalColor;
 
-    if(maximumDistance < cameraFar) finalColor = screenColor;
-    else {
+    if(screenColor == vec4(0.0)) {
         // get spherical coordinates uv for the starfield texture
         vec2 starfieldUV = vec2(
             sign(rayDir.z) * acos(rayDir.x / length(vec2(rayDir.x, rayDir.z))) / 6.28318530718,
@@ -47,8 +37,9 @@ void main() {
         // get the starfield color
         vec4 starfieldColor = texture2D(starfieldTexture, starfieldUV);
 
-        finalColor = screenColor;//vec4(starfieldColor.rgb * visibility, starfieldColor.a);
-    }
+        finalColor = vec4(starfieldColor.rgb * visibility, starfieldColor.a);
+    } else finalColor = screenColor;
 
+    
     gl_FragColor = finalColor; // displaying the final color
 }
