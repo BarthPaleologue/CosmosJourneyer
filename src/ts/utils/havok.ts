1aa29b47eddf5410fc206563af784848ba3449ae
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-08-22 20:24:03 +0200

    fixed maximum velocity changing function

--- src/ts/utils/havok.ts
+++ src/ts/utils/havok.ts
@@ -18,19 +18,27 @@ export function setEnabledBody(body: PhysicsBody, enabled: boolean, havokPlugin:
  * @param havokPlugin
  * @param maxLinVel
  * @param maxAngVel
- * @see https://forum.babylonjs.com/t/how-to-enable-faster-moving-rigidbodies-in-havok/40631/5
+ * @see https://forum.babylonjs.com/t/how-to-enable-faster-moving-rigidbodies-in-havok/40631/7
  */
-export function setMaxLinVel(maxLinVel: number, maxAngVel: number, havokPlugin: HavokPlugin) {
-    const heap = havokPlugin._hknp.HEAP8.buffer;
-    const world1 = new Int32Array(heap, Number(havokPlugin.world), 100);
-    const world2 = new Int32Array(heap, world1[9], 500);
-    const mplib = new Int32Array(heap, world2[428], 100);
-    const tsbuf = new Float32Array(heap, mplib[8], 100);
+export function setMaxLinVel(havokPlugin: HavokPlugin, maxLinVel: number, maxAngVel: number) {
+        const heap = havokPlugin._hknp.HEAP8.buffer;
+        const world1 = new Int32Array(heap, Number(havokPlugin.world), 100);
+        const world2 = new Int32Array(heap, world1[9], 500);
+        const mplib = new Int32Array(heap, world2[428], 100);
+        const tsbuf = new Float32Array(heap, mplib[8], 300);
 
-    tsbuf[32] = maxLinVel;
-    tsbuf[33] = maxAngVel;
-    tsbuf[60] = maxLinVel;
-    tsbuf[61] = maxAngVel;
-    tsbuf[88] = maxLinVel;
-    tsbuf[89] = maxAngVel;
+        tsbuf[32] = maxLinVel;
+        tsbuf[33] = maxAngVel;
+        tsbuf[60] = maxLinVel;
+        tsbuf[61] = maxAngVel;
+        tsbuf[88] = maxLinVel;
+        tsbuf[89] = maxAngVel;
+        tsbuf[144] = maxLinVel;
+        tsbuf[145] = maxAngVel;
+        tsbuf[172] = maxLinVel;
+        tsbuf[173] = maxAngVel;
+        tsbuf[200] = maxLinVel;
+        tsbuf[201] = maxAngVel;
+        tsbuf[228] = maxLinVel;
+        tsbuf[229] = maxAngVel;
 }
