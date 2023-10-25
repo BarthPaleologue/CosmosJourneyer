ab3755d18b533fc491bbcd72e0b3eed36c197a2b
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-10-25 17:55:11 +0200

    small material tweaks

--- src/ts/ui/bodyEditor/panels/gasCloudsPanel.ts
+++ src/ts/ui/bodyEditor/panels/gasCloudsPanel.ts
@@ -35,7 +35,7 @@ export class GasCloudsPanel extends EditorPanel {
         this.sliders = [
             new Slider("colorSharpness", document.getElementById("colorSharpness") as HTMLElement, 0, 100, planet.material.colorSettings.colorSharpness * 10, (val: number) => {
                 colorSettings.colorSharpness = val / 10;
-                material.updateManual();
+                material.updateConstants();
             })
         ];
     }
