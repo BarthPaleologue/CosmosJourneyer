1b3d70b03e48ccd6965b635b9a9af18d56493b96
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-10-30 16:02:39 +0100

    went back to 1:6 scale for now

--- src/ts/model/planemos/telluricPlanemoModel.ts
+++ src/ts/model/planemos/telluricPlanemoModel.ts
@@ -106,7 +106,7 @@ export class TelluricPlanemoModel implements PlanemoModel {
             max_mountain_height: 10e3,
             continent_base_height: this.physicalProperties.oceanLevel * 1.9,
 
-            mountains_frequency: (6 * (20 * this.radius)) / Settings.EARTH_RADIUS
+            mountains_frequency: (20 * this.radius) / 1000e3
         };
 
         if (this.isSatelliteOfTelluric) {
--- src/ts/settings.ts
+++ src/ts/settings.ts
@@ -1,6 +1,6 @@
 export const Settings = {
     UNIVERSE_SEED: Math.PI,
-    EARTH_RADIUS: 6000e3, // target is 6000e3
+    EARTH_RADIUS: 1000e3, // target is 6000e3
     AU: 150e9, // target is 150e9
     VERTEX_RESOLUTION: 64,
     CLOUD_LAYER_HEIGHT: 15e3,
