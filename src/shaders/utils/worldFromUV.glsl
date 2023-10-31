79ac9dbe906881921d2258bb674be1954fbb50c3
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-10-31 19:57:14 +0100

    Fixed unprojection
    The reverse screen projection is now solved thanks to the awesome discussion at https://forum.babylonjs.com/t/reverse-depth-buffer-question/45318

--- src/shaders/utils/worldFromUV.glsl
+++ src/shaders/utils/worldFromUV.glsl
@@ -1,7 +1,9 @@
 // compute the world position of a pixel from its uv coordinates
+// This is an evolution from the code found here
 // https://forum.babylonjs.com/t/pixel-position-in-world-space-from-fragment-postprocess-shader-issue/30232
+// This is a revised version that works with the reverse depth buffer
 vec3 worldFromUV(vec2 pos) {
-    vec4 ndc = vec4(pos.xy * 2.0 - 1.0, -1.0, 1.0); // get ndc position -1 because i want every point in the near camera plane
+    vec4 ndc = vec4(-pos.xy * 2.0 + 1.0, -1.0, -1.0); // get ndc position (z = -1 because the depth buffer is reversed)
     vec4 posVS = inverseProjection * ndc; // unproject the ndc coordinates : we are now in view space if i understand correctly
     vec4 posWS = inverseView * vec4((posVS.xyz / posVS.w), 1.0); // then we use inverse view to get to world space, division by w to get actual coordinates
     return posWS.xyz; // the coordinates in world space
