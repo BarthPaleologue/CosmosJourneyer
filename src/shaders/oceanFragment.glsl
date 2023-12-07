df69ff0f0b11c823829dc9f57ae7741c75da6e78
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-12-07 15:00:04 +0100

    working on WebGPU compatibility

--- src/shaders/oceanFragment.glsl
+++ src/shaders/oceanFragment.glsl
@@ -1,4 +1,4 @@
-precision lowp float;
+precision highp float;
 
 /* disable_uniformity_analysis */
 
@@ -41,75 +41,6 @@ uniform float time;
 
 #include "./utils/computeSpecularHighlight.glsl";
 
-#define inline
-vec4 oceanColor(vec4 originalColor, vec3 rayOrigin, vec3 rayDir, float maximumDistance) {
-    float impactPoint, escapePoint;
-
-    float waveAmplitude = 20.0;
-
-    float waveOmega = 1.0/7.0;
-
-    float actualRadius = ocean_radius + waveAmplitude * sin(time * waveOmega);
-
-    if (!(rayIntersectSphere(rayOrigin, rayDir, object_position, actualRadius, impactPoint, escapePoint))) {
-        return originalColor;// if not intersecting with atmosphere, return original color
-    }
-
-    impactPoint = max(0.0, impactPoint);// cannot be negative (the ray starts where the camera is in such a case)
-    escapePoint = min(maximumDistance, escapePoint);// occlusion with other scene objects
-
-    float distanceThroughOcean = max(0.0, escapePoint - impactPoint);// probably doesn't need the max but for the sake of coherence the distance cannot be negative
-
-    if (distanceThroughOcean <= 0.0) return originalColor;
-
-    vec3 samplePoint = rayOrigin + impactPoint * rayDir - object_position;
-
-    vec3 samplePointPlanetSpace = applyQuaternion(planetInverseRotationQuaternion, samplePoint);
-
-    vec3 unitSamplePoint = normalize(samplePointPlanetSpace);
-
-    vec3 planetNormal = normalize(samplePoint);
-
-    vec3 normalWave = triplanarNormal(samplePointPlanetSpace + vec3(time, time, -time) * 100.0, planetNormal, normalMap2, 0.00015, ocean_waveBlendingSharpness, 1.0);
-    normalWave = triplanarNormal(samplePointPlanetSpace + vec3(-time, time, -time) * 100.0, normalWave, normalMap1, 0.0001, ocean_waveBlendingSharpness, 1.0);
-
-    normalWave = triplanarNormal(samplePointPlanetSpace + vec3(time, -time, -time) * 300.0, normalWave, normalMap1, 0.000025, ocean_waveBlendingSharpness, 0.5);
-    normalWave = triplanarNormal(samplePointPlanetSpace + vec3(-time, -time, time) * 300.0, normalWave, normalMap2, 0.00002, ocean_waveBlendingSharpness, 0.5);
-
-    normalWave = triplanarNormal(samplePointPlanetSpace + vec3(time, -time, -time) * 500.0, normalWave, normalMap2, 0.000010, ocean_waveBlendingSharpness, 0.5);
-    normalWave = triplanarNormal(samplePointPlanetSpace + vec3(-time, -time, time) * 500.0, normalWave, normalMap1, 0.000005, ocean_waveBlendingSharpness, 0.5);
-
-    float opticalDepth01 = 1.0 - exp(-distanceThroughOcean * ocean_depthModifier);
-    float alpha = exp(-distanceThroughOcean * ocean_alphaModifier);
-
-    //vec3 oceanColor = lerp(vec3(10.0, 100.0, 249.0)/255.0, vec3(15.0,94.0,156.0)/255.0, opticalDepth01);
-
-    vec3 deepColor = vec3(0.0, 22.0, 82.0)/255.0;
-    vec3 shallowColor = vec3(32.0, 193.0, 180.0)/255.0;
-    vec3 oceanColor = mix(shallowColor, deepColor, opticalDepth01) * star_colors[0];
-
-    vec3 ambiant = mix(oceanColor, originalColor.rgb, alpha);
-
-    float foamSize = 30.0;
-    float foamFactor = saturate((foamSize - distanceThroughOcean) / foamSize);
-    vec3 foamColor = vec3(0.8);
-    ambiant = mix(ambiant, foamColor, foamFactor);
-
-    vec3 finalColor = vec3(0.0);
-    for (int i = 0; i < nbStars; i++) {
-        vec3 sunDir = normalize(star_positions[i] - samplePoint);
-
-        float ndl = max(dot(planetNormal, sunDir), 0.0);
-        finalColor += ambiant * ndl;
-
-        if (length(rayOrigin - object_position) > ocean_radius) {
-            // if above ocean surface then specular highlight
-            finalColor += computeSpecularHighlight(sunDir, rayDir, normalWave, ocean_smoothness, ocean_specularPower) * star_colors[i];
-        }
-    }
-
-    return vec4(finalColor, 1.0);
-}
 
 void main() {
     vec4 screenColor = texture2D(textureSampler, vUV);// the current screen color
@@ -123,7 +54,61 @@ void main() {
 
     vec3 rayDir = normalize(pixelWorldPosition - camera_position);// normalized direction of the ray
 
-    vec4 finalColor = oceanColor(screenColor, camera_position, rayDir, maximumDistance);
+    vec4 finalColor = screenColor;
 
-    gl_FragColor = finalColor;// displaying the final color
+    float waveAmplitude = 20.0;
+    float waveOmega = 1.0/7.0;
+    float actualRadius = ocean_radius + waveAmplitude * sin(time * waveOmega);
+
+    float impactPoint, escapePoint;
+    if (rayIntersectSphere(camera_position, rayDir, object_position, actualRadius, impactPoint, escapePoint) && impactPoint < maximumDistance) {
+        impactPoint = max(0.0, impactPoint);// cannot be negative (the ray starts where the camera is in such a case)
+        escapePoint = min(maximumDistance, escapePoint);// occlusion with other scene objects
+
+        float distanceThroughOcean = max(0.0, escapePoint - impactPoint);// probably doesn't need the max but for the sake of coherence the distance cannot be negative
+
+        vec3 samplePoint = camera_position + impactPoint * rayDir - object_position;
+
+        vec3 samplePointPlanetSpace = applyQuaternion(planetInverseRotationQuaternion, samplePoint);
+        vec3 unitSamplePoint = normalize(samplePointPlanetSpace);
+        vec3 planetNormal = normalize(samplePoint);
+
+        vec3 normalWave = triplanarNormal(samplePointPlanetSpace + vec3(time, time, -time) * 100.0, planetNormal, normalMap2, 0.00015, ocean_waveBlendingSharpness, 1.0);
+        normalWave = triplanarNormal(samplePointPlanetSpace + vec3(-time, time, -time) * 100.0, normalWave, normalMap1, 0.0001, ocean_waveBlendingSharpness, 1.0);
+
+        normalWave = triplanarNormal(samplePointPlanetSpace + vec3(time, -time, -time) * 300.0, normalWave, normalMap1, 0.000025, ocean_waveBlendingSharpness, 0.5);
+        normalWave = triplanarNormal(samplePointPlanetSpace + vec3(-time, -time, time) * 300.0, normalWave, normalMap2, 0.00002, ocean_waveBlendingSharpness, 0.5);
+
+        normalWave = triplanarNormal(samplePointPlanetSpace + vec3(time, -time, -time) * 500.0, normalWave, normalMap2, 0.000010, ocean_waveBlendingSharpness, 0.5);
+        normalWave = triplanarNormal(samplePointPlanetSpace + vec3(-time, -time, time) * 500.0, normalWave, normalMap1, 0.000005, ocean_waveBlendingSharpness, 0.5);
+
+        float opticalDepth01 = 1.0 - exp(-distanceThroughOcean * ocean_depthModifier);
+        float alpha = exp(-distanceThroughOcean * ocean_alphaModifier);
+
+        vec3 deepColor = vec3(0.0, 22.0, 82.0)/255.0;
+        vec3 shallowColor = vec3(32.0, 193.0, 180.0)/255.0;
+        vec3 oceanColor = mix(shallowColor, deepColor, opticalDepth01) * star_colors[0];
+
+        vec3 ambiant = mix(oceanColor, screenColor.rgb, alpha);
+
+        float foamSize = 30.0;
+        float foamFactor = saturate((foamSize - distanceThroughOcean) / foamSize);
+        vec3 foamColor = vec3(0.8);
+        ambiant = mix(ambiant, foamColor, foamFactor);
+
+        finalColor.rgb = vec3(0.0);
+        for (int i = 0; i < nbStars; i++) {
+            vec3 sunDir = normalize(star_positions[i] - samplePoint);
+
+            float ndl = max(dot(planetNormal, sunDir), 0.0);
+            finalColor.rgb += ambiant * ndl;
+
+            if (length(camera_position - object_position) > ocean_radius) {
+                // if above ocean surface then specular highlight
+                finalColor.rgb += computeSpecularHighlight(sunDir, rayDir, normalWave, ocean_smoothness, ocean_specularPower) * star_colors[i];
+            }
+        }
+    }
+
+    gl_FragColor = finalColor;
 }
