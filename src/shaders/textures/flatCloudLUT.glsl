da6dc0efe89676d1ab643a8c8518ef4837893a31
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-12-20 19:13:14 +0100

    improved clouds lut
    The lut contains directly the shape density, so there is no need for 1.0 - worley in the final shader

--- src/shaders/textures/flatCloudLUT.glsl
+++ src/shaders/textures/flatCloudLUT.glsl
@@ -14,8 +14,8 @@ uniform float detailFrequency;
 void main() {
     vec3 sphere = toSphere(vUV);
 
-    float worley = completeWorley(sphere * worleyFrequency, 1, 2.0, 2.0);
-    float detailNoise = completeNoise(sphere * detailFrequency, 5, 2.0, 2.0);
+    float worley = completeWorley(sphere * worleyFrequency, 4, 2.0, 2.0);
+    float detailNoise = completeNoise(sphere * detailFrequency, 8, 2.0, 2.0);
 
-    gl_FragColor = vec4(vec3(worley, detailNoise, 0.0), 1.0);
+    gl_FragColor = vec4(vec3(1.0 - worley, detailNoise, 0.0), 1.0);
 }
