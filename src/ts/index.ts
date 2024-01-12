99bd658a2f177e97b01a475d89b12ef295040d29
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-01-13 00:09:49 +0100

    small visual adjustments

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -121,7 +121,7 @@ terminaModel.orbit.period = 60 * 60;
 const termina = StarSystemHelper.makeStar(starSystem, terminaModel);*/
 
 const planetModel = new TelluricPlanemoModel(0.4233609183800225, sunModel);
-planetModel.physicalProperties.minTemperature = -37;
+planetModel.physicalProperties.minTemperature = -55;
 planetModel.physicalProperties.maxTemperature = 30;
 
 planetModel.orbit.period = 60 * 60 * 24 * 365.25;
