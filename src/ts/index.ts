17183df47e3ccc9f2dbf5daf4b21557948d849b1
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-01-20 22:51:28 +0100

    stripped debug system from main entry point

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -17,31 +17,17 @@
 
 import "../styles/index.scss";
 
-import { StarSystemController } from "./starSystem/starSystemController";
-
 import { Settings } from "./settings";
-import { Assets } from "./assets";
 import { DefaultControls } from "./defaultController/defaultControls";
-import { positionNearObjectBrightSide } from "./utils/positionNearObject";
 import { CosmosJourneyer } from "./cosmosJourneyer";
-import { Vector3 } from "@babylonjs/core/Maths/math.vector";
-import { Color3 } from "@babylonjs/core/Maths/math.color";
 import { ShipControls } from "./spaceship/shipControls";
-import { PostProcessType } from "./postProcesses/postProcessTypes";
-import { TelluricPlanetModel } from "./planets/telluricPlanet/telluricPlanetModel";
-import { GasPlanetModel } from "./planets/gasPlanet/gasPlanetModel";
 import { getForwardDirection, getRotationQuaternion, setRotationQuaternion, translate } from "./uberCore/transforms/basicTransform";
 import { parsePercentageFrom01, parseSpeed } from "./utils/parseToStrings";
-
-import { StarSystemHelper } from "./starSystem/starSystemHelper";
 import { Mouse } from "./inputs/mouse";
 import { Keyboard } from "./inputs/keyboard";
-import { StarModel } from "./stellarObjects/star/starModel";
-import { RingsUniforms } from "./postProcesses/rings/ringsUniform";
-import { getMoonSeed } from "./planets/common";
-
 import { Gamepad } from "./inputs/gamepad";
 import { CharacterControls } from "./spacelegs/characterControls";
+import { StarSystemController } from "./starSystem/starSystemController";
 import { SystemSeed } from "./utils/systemSeed";
 
 const engine = new CosmosJourneyer();
@@ -114,135 +100,13 @@ engine.onToggleStarMapObservable.add((isStarMapOpen) => {
     if (!isStarMapOpen) spaceshipController.thirdPersonCamera.radius = 30;
 });
 
-console.log(`Time is going ${Settings.TIME_MULTIPLIER} time${Settings.TIME_MULTIPLIER > 1 ? "s" : ""} faster than in reality`);
-
-const starSystemSeed = new SystemSeed(0, 0, 0, 0);
-const starSystem = new StarSystemController(starSystemSeed, starSystemView.scene);
-starSystem.model.setName("Alpha Testis");
-
-engine.getStarSystemView().setStarSystem(starSystem, false);
-
-const sunModel = new StarModel(0.51);
-const sun = StarSystemHelper.makeStar(starSystem, sunModel);
-sun.model.orbit.period = 60 * 60 * 24;
-
-/*const secundaModel = new StarModel(-672446, sunModel);
-secundaModel.orbit.radius = 30 * sunModel.radius;
-secundaModel.orbit.period = 60 * 60;
-const secunda = StarSystemHelper.makeStar(starSystem, secundaModel);
-
-const terminaModel = new StarModel(756263, sunModel);
-terminaModel.orbit.radius = 50 * sunModel.radius;
-terminaModel.orbit.period = 60 * 60;
-const termina = StarSystemHelper.makeStar(starSystem, terminaModel);*/
-
-const planetModel = new TelluricPlanetModel(0.4233609183800225, sunModel);
-planetModel.physicalProperties.minTemperature = -55;
-planetModel.physicalProperties.maxTemperature = 30;
-
-planetModel.orbit.period = 60 * 60 * 24 * 365.25;
-planetModel.orbit.radius = 4000 * planetModel.radius;
-planetModel.orbit.normalToPlane = Vector3.Up();
-
-const planet = StarSystemHelper.makeTelluricPlanet(starSystem, planetModel);
-planet.model.ringsUniforms = new RingsUniforms(planet.model.rng);
-planet.postProcesses.push(PostProcessType.RING);
-
-//const spacestation = new SpaceStation(starSystemView.scene, planet);
-//starSystemView.getStarSystem().addSpaceStation(spacestation);
-
-const moonModel = new TelluricPlanetModel(getMoonSeed(planetModel, 0), planetModel);
-moonModel.physicalProperties.mass = 2;
-moonModel.physicalProperties.rotationPeriod = 7 * 60 * 60;
-moonModel.physicalProperties.minTemperature = -180;
-moonModel.physicalProperties.maxTemperature = 200;
-moonModel.physicalProperties.waterAmount = 0.9;
-
-moonModel.orbit.period = moonModel.physicalProperties.rotationPeriod;
-moonModel.orbit.radius = 8 * planet.getRadius();
-moonModel.orbit.normalToPlane = Vector3.Up();
-
-const moon = StarSystemHelper.makeSatellite(starSystem, planet, moonModel);
-
-moon.material.colorSettings.plainColor.copyFromFloats(0.67, 0.67, 0.67);
-moon.material.colorSettings.desertColor.copyFrom(new Color3(116, 134, 121).scale(1 / 255));
-moon.material.colorSettings.steepColor.copyFrom(new Color3(92, 92, 92).scale(1 / 255));
-
-moon.material.setTexture("plainNormalMap", Assets.DirtNormalMap);
-moon.material.setTexture("bottomNormalMap", Assets.DirtNormalMap);
-moon.material.updateConstants();
-
-const aresModel = new TelluricPlanetModel(0.3725, sunModel);
-aresModel.physicalProperties.mass = 7;
-aresModel.physicalProperties.rotationPeriod = (24 * 60 * 60) / 30;
-aresModel.physicalProperties.minTemperature = -30;
-aresModel.physicalProperties.maxTemperature = 20;
-aresModel.physicalProperties.pressure = 0.5;
-aresModel.physicalProperties.waterAmount = 0.2;
-aresModel.physicalProperties.oceanLevel = 0;
-
-aresModel.orbit.period = 60 * 60 * 24 * 365.24;
-aresModel.orbit.radius = 4020 * planet.getRadius();
-aresModel.orbit.normalToPlane = Vector3.Up();
-
-//aresModel.terrainSettings.continents_fragmentation = 0.0;
-//aresModel.terrainSettings.continent_base_height = 10e3;
-//aresModel.terrainSettings.max_mountain_height = 20e3;
-
-const ares = StarSystemHelper.makeTelluricPlanet(starSystem, aresModel);
-ares.postProcesses.splice(ares.postProcesses.indexOf(PostProcessType.OCEAN), 1);
-ares.postProcesses.splice(ares.postProcesses.indexOf(PostProcessType.CLOUDS), 1);
-
-ares.material.colorSettings.plainColor.copyFromFloats(139 / 255, 59 / 255, 24 / 255);
-ares.material.colorSettings.desertColor.copyFromFloats(178 / 255, 107 / 255, 42 / 255);
-ares.material.colorSettings.beachColor.copyFrom(ares.material.colorSettings.plainColor);
-ares.material.colorSettings.bottomColor.copyFrom(ares.material.colorSettings.plainColor.scale(0.9));
-
-ares.material.updateConstants();
-
-const andromaqueModel = new GasPlanetModel(0.28711440474126226, sunModel);
-andromaqueModel.orbit.period = 60 * 60 * 24 * 365.25;
-andromaqueModel.orbit.radius = 4300 * ares.getRadius();
-andromaqueModel.orbit.normalToPlane = Vector3.Up();
-
-const andromaque = StarSystemHelper.makeGasPlanet(starSystem, andromaqueModel);
-
-/*const blackHoleModel = new BlackHoleModel(0.5, sunModel);
-blackHoleModel.orbit.period = 60 * 60 * 24 * 365.25;
-blackHoleModel.orbit.radius = 100 * ares.getRadius();
-const blackHole = starSystem.makeBlackHole(blackHoleModel);*/
+const starSystem = new StarSystemController(new SystemSeed(0, 0, 0, 0), starSystemView.scene);
+starSystemView.setStarSystem(starSystem, true);
 
 engine.init();
 
-positionNearObjectBrightSide(starSystemView.scene.getActiveController(), planet, starSystem, 2);
-
-spaceshipController.toggleWarpDrive();
-
-const aresAtmosphere = starSystem.postProcessManager.getAtmosphere(ares);
-if (aresAtmosphere) {
-    aresAtmosphere.atmosphereUniforms.redWaveLength = 500;
-    aresAtmosphere.atmosphereUniforms.greenWaveLength = 680;
-    aresAtmosphere.atmosphereUniforms.blueWaveLength = 670;
-} else {
-    console.warn("No atmosphere found for Ares");
-}
-
 document.addEventListener("keydown", (e) => {
     if (engine.isPaused()) return;
-
-    if (e.key === "x") {
-        let nbVertices = 0;
-        let nbInstances = 0;
-        planet.sides.forEach((side) => {
-            side.executeOnEveryChunk((chunk) => {
-                nbVertices += Settings.VERTEX_RESOLUTION * Settings.VERTEX_RESOLUTION;
-                chunk.instancePatches.forEach((patch) => {
-                    nbInstances += patch.getNbInstances();
-                });
-            });
-        });
-        console.log("Vertices", nbVertices, "Instances", nbInstances);
-    }
 
     if (e.key === "y") {
         if (starSystemView.scene.getActiveController() === spaceshipController) {
@@ -281,7 +145,7 @@ document.addEventListener("keydown", (e) => {
             starSystemView.getStarSystem().postProcessManager.rebuild();
 
             spaceshipController.setEnabled(false, starSystemView.havokPlugin);
-        } else if(scene.getActiveController() === characterController) {
+        } else if (scene.getActiveController() === characterController) {
             characterController.getTransform().setEnabled(false);
             scene.setActiveController(spaceshipController);
             setRotationQuaternion(spaceshipController.getTransform(), getRotationQuaternion(defaultController.getTransform()).clone());
