b97db0cb529f5014081b61dfe2bdfcffd7ac5921
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-01-22 10:20:22 +0100

    broken accretion disks

--- src/ts/blackHoleDemo.ts
+++ src/ts/blackHoleDemo.ts
@@ -19,19 +19,9 @@ import "../styles/index.scss";
 
 import { StarSystemController } from "./starSystem/starSystemController";
 
-import { randRange } from "extended-random";
-import { Settings } from "./settings";
-import { DefaultControls } from "./defaultController/defaultControls";
 import { positionNearObjectBrightSide } from "./utils/positionNearObject";
 import { CosmosJourneyer } from "./cosmosJourneyer";
-import { ShipControls } from "./spaceship/shipControls";
-import { getRotationQuaternion, setRotationQuaternion } from "./uberCore/transforms/basicTransform";
-import { parsePercentageFrom01, parseSpeed } from "./utils/parseToStrings";
 import { StarSystemHelper } from "./starSystem/starSystemHelper";
-import { Mouse } from "./inputs/mouse";
-import { Keyboard } from "./inputs/keyboard";
-import { Gamepad } from "./inputs/gamepad";
-import { Vector3 } from "@babylonjs/core/Maths/math.vector";
 import { SystemSeed } from "./utils/systemSeed";
 
 const engine = new CosmosJourneyer();
@@ -41,44 +31,6 @@ await engine.setup();
 const starSystemView = engine.getStarSystemView();
 
 const scene = starSystemView.scene;
-
-const mouse = new Mouse(engine.canvas, 100);
-const keyboard = new Keyboard();
-const gamepad = new Gamepad();
-
-const player = new DefaultControls(scene);
-player.speed = 0.2 * Settings.EARTH_RADIUS;
-player.getActiveCamera().maxZ = Settings.EARTH_RADIUS * 100000;
-player.addInput(keyboard);
-player.addInput(gamepad);
-
-const spaceshipController = new ShipControls(scene);
-spaceshipController.getActiveCamera().maxZ = Settings.EARTH_RADIUS * 100000;
-spaceshipController.addInput(keyboard);
-spaceshipController.addInput(mouse);
-spaceshipController.addInput(gamepad);
-
-scene.setActiveController(spaceshipController);
-
-engine.registerStarSystemUpdateCallback(() => {
-    if (scene.getActiveController() != spaceshipController) return;
-
-    const shipPosition = spaceshipController.getTransform().getAbsolutePosition();
-    const nearestBody = starSystemView.getStarSystem().getNearestOrbitalObject();
-    const distance = nearestBody.getTransform().getAbsolutePosition().subtract(shipPosition).length();
-    const radius = nearestBody.getBoundingRadius();
-    spaceshipController.registerClosestObject(distance, radius);
-
-    const warpDrive = spaceshipController.getWarpDrive();
-    const shipInternalThrottle = warpDrive.getInternalThrottle();
-    const shipTargetThrottle = warpDrive.getTargetThrottle();
-
-    const throttleString = warpDrive.isEnabled()
-        ? `${parsePercentageFrom01(shipInternalThrottle)}/${parsePercentageFrom01(shipTargetThrottle)}`
-        : `${parsePercentageFrom01(spaceshipController.getThrottle())}/100%`;
-
-    (document.querySelector("#speedometer") as HTMLElement).innerHTML = `${throttleString} | ${parseSpeed(spaceshipController.getSpeed())}`;
-});
 
 const starSystemSeed = new SystemSeed(0, 0, 0, 0);
 const starSystem = new StarSystemController(starSystemSeed, scene);
@@ -90,27 +42,10 @@ BH.model.orbit.radius = 0;
 const planet = StarSystemHelper.makeTelluricPlanet(starSystem);
 planet.model.orbit.radius = 45 * planet.getRadius();
 
-document.addEventListener("keydown", (e) => {
-    if (engine.isPaused()) return;
-    if (e.key === "g") {
-        if (scene.getActiveController() === spaceshipController) {
-            scene.setActiveController(player);
-            setRotationQuaternion(player.getTransform(), getRotationQuaternion(spaceshipController.getTransform()).clone());
-            starSystemView.getStarSystem().postProcessManager.rebuild();
-
-            spaceshipController.setEnabled(false, starSystemView.havokPlugin);
-        } else {
-            scene.setActiveController(spaceshipController);
-            setRotationQuaternion(spaceshipController.getTransform(), getRotationQuaternion(player.getTransform()).clone());
-            starSystemView.getStarSystem().postProcessManager.rebuild();
-
-            spaceshipController.setEnabled(true, starSystemView.havokPlugin);
-        }
-    }
-});
-
-engine.init();
+engine.init(true);
 
 positionNearObjectBrightSide(scene.getActiveController(), BH, starSystem, 20);
 
-engine.toggleStarMap();
+starSystemView.ui.setEnabled(true);
+starSystemView.showUI();
+starSystemView.getSpaceshipControls().enableWarpDrive();
