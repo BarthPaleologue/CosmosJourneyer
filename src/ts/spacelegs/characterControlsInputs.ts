797a0a60b29cb7a1bd4713ac1aab4467cb3ea9fa
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-02-17 14:55:04 +0100

    ported character controls to new input system

---
+++ src/ts/spacelegs/characterControlsInputs.ts
@@ -0,0 +1,55 @@
+import DPadComposite from "@brianchirls/game-input/controls/DPadComposite";
+import Action from "@brianchirls/game-input/Action";
+import PressInteraction from "@brianchirls/game-input/interactions/PressInteraction";
+import { InputDevices } from "../input";
+
+const gamepad = InputDevices.gamepad;
+const keyboard = InputDevices.keyboard;
+
+const leftStick = gamepad.getControl("leftStick");
+
+// It takes four keys to go in four directions
+const kbdWASD = new DPadComposite({
+  up: keyboard.getControl("KeyW"),
+  left: keyboard.getControl("KeyA"),
+  down: keyboard.getControl("KeyS"),
+  right: keyboard.getControl("KeyD")
+});
+
+/**
+ * Combine controls into actions.
+ * The action will respond to whichever control is used.
+ */
+const moveAction = new Action({
+  bindings: [leftStick, kbdWASD]
+});
+
+const jumpKey = keyboard.getControl("Space");
+const jumpButton = gamepad.getControl("A");
+
+const jumpAction = new Action({
+  bindings: [jumpKey, jumpButton]
+});
+
+const jumpInteraction = new PressInteraction(jumpAction);
+
+const sambaKey = keyboard.getControl("KeyX");
+const sambaButton = gamepad.getControl("X");
+
+const sambaAction = new Action({
+  bindings: [sambaKey, sambaButton]
+});
+
+const runKey = keyboard.getControl("ShiftLeft");
+const runButton = gamepad.getControl("B");
+
+const runAction = new Action({
+  bindings: [runKey, runButton]
+});
+
+export const CharacterInputs = {
+  move: moveAction,
+  jump: jumpInteraction,
+  samba: sambaAction,
+  run: runAction
+};
