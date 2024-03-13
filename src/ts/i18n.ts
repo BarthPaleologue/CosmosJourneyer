a55e266adc50b7f5498ee5df2587d7fb5850302f
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-03-13 19:56:27 +0100

    Complete i18n setup
    - automatically load all translation files at compile time
    - changed folder names to match exactly the languages codes
    - added translation section to contribute panel
    - added translation guide to contribute markdown

--- src/ts/i18n.ts
+++ src/ts/i18n.ts
@@ -1,33 +1,34 @@
-import i18next from 'i18next';
+import i18next, { Resource, init } from "i18next";
 
-import enCommon from '../locales/en-us/common.json';
-import enMainMenu from '../locales/en-us/mainMenu.json';
-import enPauseMenu from '../locales/en-us/pauseMenu.json';
-import enNotifications from '../locales/en-us/notifications.json';
+/**
+ * Load all the resources from the locales folder and return them in the i18next format.
+ * This takes place at build time, so the resources are bundled with the application.
+ */
+function loadResources() {
+    const requireContext = require.context("../locales/", true, /\.json$/);
+    const resources: Resource = {}; // { "en-US": { "notifications": { ... } }, "es-ES": { "notifications": { ... } } }
 
-import frCommon from '../locales/fr-fr/common.json';
-import frMainMenu from '../locales/fr-fr/mainMenu.json';
-import frPauseMenu from '../locales/fr-fr/pauseMenu.json';
-import frNotifications from '../locales/fr-fr/notifications.json';
+    requireContext.keys().forEach((key: string) => {
+        const languageFolder = key.split("/")[1]; // (./en-US/notifications.json) => en-US
+        const nameSpace = key.split("/")[2].split(".")[0]; // (./en-US/notifications.json) => notifications
+        const fileContent = requireContext(key);
 
-await i18next.init({
-    lng: navigator.language, // if you're using a language detector, do not define the lng option
+        resources[languageFolder] = resources[languageFolder] || {};
+        resources[languageFolder][nameSpace] = fileContent;
+    });
+
+    return resources;
+}
+
+// init language to url parameter if defined, otherwise use the browser language
+const urlParams = new URLSearchParams(window.location.search);
+const language = urlParams.get("lang") || navigator.language;
+
+await init({
+    lng: language, // change this if you want to test a specific language
     debug: true,
-    fallbackLng: 'en-US',
-    resources: {
-        "en-US": {
-            common: enCommon,
-            mainMenu: enMainMenu,
-            pauseMenu: enPauseMenu,
-            notifications: enNotifications
-        },
-        "fr-FR": {
-            common: frCommon,
-            mainMenu: frMainMenu,
-            pauseMenu: frPauseMenu,
-            notifications: frNotifications
-        }
-    }
+    fallbackLng: "en-US",
+    resources: loadResources()
 });
 
 export default i18next;
