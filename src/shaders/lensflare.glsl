3202e918d1f57f7067b6861096957ef616fbb2db
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-03-24 21:39:35 +0100

    Switch engine to WebGPU
    world from uv now directly takes the non linear depth from the depth renderer. (#61)
    
    Although it works better, there is still an issue with the clear color
    
    Fix shader compilation errors
    
    - fix visual issue by computing rayDir separately
    - half the number of matrix inversion by inverting the projection view product
    
    giving up on supporting WebGL #61
    
    Supporting both WebGL and WebGPU is a pain so this branch will assume a WebGPU only future
    
    Add missing engine init options
    
    trying to fix materials for WebGPU
    
    fixed glsl compilation error
    
    Update grassVertex.glsl
    
    Fix stride error in ShaderMaterial setArray3 => use setColor3 instead

--- src/shaders/lensflare.glsl
+++ src/shaders/lensflare.glsl
@@ -108,8 +108,8 @@ void main() {
         return;
     }
 
-    vec3 pixelWorldPosition = worldFromUV(vUV, camera_inverseProjection, camera_inverseView);// the pixel position in world space (near plane)
-    vec3 rayDir = normalize(pixelWorldPosition - camera_position);
+    vec3 pixelWorldPosition = worldFromUV(vUV, 1.0, camera_inverseProjectionView);// the pixel position in world space (near plane)
+    vec3 rayDir = normalize(worldFromUV(vUV, 1.0, camera_inverseProjectionView) - camera_position);
 
     vec3 objectDirection = normalize(object_position - camera_position);
 
