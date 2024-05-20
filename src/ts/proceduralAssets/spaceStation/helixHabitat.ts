e66ed0c879da66000a693ca7e1769905473f7c66
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-05-20 16:33:28 +0200

    added arms to helix habitat

--- src/ts/proceduralAssets/spaceStation/helixHabitat.ts
+++ src/ts/proceduralAssets/spaceStation/helixHabitat.ts
@@ -8,21 +8,81 @@ import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
 export function createHelixHabitat(scene: Scene): TransformNode {
     const root = new TransformNode("HelixHabitatRoot");
 
-    const nbSpires = 2;
+    const nbSpires = 2 + Math.floor(Math.random() * 4)
 
-    const pitch = 4e3;
+    const pitch = 4e3 * (1 + 0.3 * (Math.random() * 2 - 1))
 
-    const helix1 = createHelix("HelixHabitat", { radius: 2e3, tubeDiameter: 100, tessellation: 32, pitch: pitch, spires: nbSpires }, scene);
-    const helix2 = createHelix("HelixHabitat", { radius: 2e3, tubeDiameter: 100, tessellation: 32, pitch: pitch, spires: nbSpires }, scene);
+    const radius = 2e3 + Math.random() * 2e3;
+
+    const tubeDiameter = 100 + Math.random() * 100;
+
+    const tesselation = 32;
+
+    const totalLength = pitch * nbSpires;
+
+    const attachmentNbSides = 6 + 2 * Math.floor(Math.random() * 2);
+
+    const attachment = MeshBuilder.CreateCylinder(
+        "HelixHabitatAttachment",
+        {
+            diameterTop: 100,
+            diameterBottom: 100,
+            height: totalLength,
+            tessellation: attachmentNbSides
+        },
+        scene
+    );
+    attachment.rotate(Axis.Y, Math.PI / attachmentNbSides, Space.WORLD);
+    attachment.parent = root;
+
+    const helix1 = createHelix(
+        "HelixHabitat",
+        {
+            radius: radius,
+            tubeDiameter: tubeDiameter,
+            tessellation: tesselation,
+            pitch: pitch,
+            spires: nbSpires
+        },
+        scene
+    );
+    const helix2 = createHelix(
+        "HelixHabitat",
+        {
+            radius: radius,
+            tubeDiameter: tubeDiameter,
+            tessellation: tesselation,
+            pitch: pitch,
+            spires: nbSpires
+        },
+        scene
+    );
     helix2.rotate(Axis.Y, Math.PI, Space.WORLD);
 
     helix1.parent = root;
     helix2.parent = root;
 
-    const totalLength = pitch * nbSpires;
+    const nbArms = (attachmentNbSides * nbSpires) / 2;
+    for (let i = 0; i <= nbArms; i++) {
+        const arm = MeshBuilder.CreateBox(
+            "HelixHabitatArm",
+            {
+                width: 2 * radius,
+                depth: tubeDiameter / 3,
+                height: tubeDiameter / 3
+            },
+            scene
+        );
 
-    const attachment = MeshBuilder.CreateCylinder("HelixHabitatAttachment", { diameterTop: 100, diameterBottom: 100, height: totalLength, tessellation: 6 }, scene);
-    attachment.parent = root;
+        const y = (i / nbArms) * totalLength - totalLength / 2;
+
+        const theta = (y / pitch) * Math.PI * 2;
+
+        arm.position.y = y;
+        arm.rotate(Axis.Y, theta, Space.WORLD);
+
+        arm.parent = root;
+    }
 
     return root;
 }
