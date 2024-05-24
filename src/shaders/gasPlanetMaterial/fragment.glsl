f65b1e1d12005e028f5037442be272ef370a1a8c
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-05-24 10:03:31 +0200

    more accurate gas planet shading

--- src/shaders/gasPlanetMaterial/fragment.glsl
+++ src/shaders/gasPlanetMaterial/fragment.glsl
@@ -58,38 +58,35 @@ void main() {
         ndl = max(ndl, max(0.0, dot(normalW, starLightRayW)));
     }
 
+    vec4 seededSamplePoint = vec4(vUnitSamplePoint * 2.0, mod(seed, 1e3));
+
+    seededSamplePoint.y *= 2.5;
+
+    float latitude = seededSamplePoint.y;
+
+    float seedImpact = mod(seed, 1e3);
+
+    float warpingStrength = 2.0;
+    float warping = fractalSimplex4(seededSamplePoint + vec4(seedImpact, 0.0, 0.0, time * 0.01), 5, 2.0, 2.0) * warpingStrength;
+
+    float colorDecision1 = fractalSimplex4(vec4(latitude + warping, seedImpact, -seedImpact, seedImpact), 3, 2.0, 2.0);
+
+    float colorDecision2 = fractalSimplex4(vec4(latitude - warping, seedImpact, -seedImpact, seedImpact), 3, 2.0, 2.0);
+
+    vec3 albedo = mix(color2, color1, smoothstep(0.4, 0.6, colorDecision1));
+
+    albedo = 3.0 * mix(color3, albedo, smoothSharpener(colorDecision2, colorSharpness));
+
+    float roughness = 0.4;
+    float metallic = 0.0;
+
+    // pbr accumulation
     vec3 Lo = vec3(0.0);
+    vec3 viewRayW = normalize(cameraPosition - vPositionW);
+    for (int i = 0; i < nbStars; i++) {
+        vec3 L = normalize(star_positions[i] - vPositionW);
 
-    if (ndl > 0.0) {
-        vec4 seededSamplePoint = vec4(vUnitSamplePoint * 2.0, mod(seed, 1e3));
-
-        seededSamplePoint.y *= 2.5;
-
-        float latitude = seededSamplePoint.y;
-
-        float seedImpact = mod(seed, 1e3);
-
-        float warpingStrength = 2.0;
-        float warping = fractalSimplex4(seededSamplePoint + vec4(seedImpact, 0.0, 0.0, time * 0.01), 5, 2.0, 2.0) * warpingStrength;
-
-        float colorDecision1 = fractalSimplex4(vec4(latitude + warping, seedImpact, -seedImpact, seedImpact), 3, 2.0, 2.0);
-
-        float colorDecision2 = fractalSimplex4(vec4(latitude - warping, seedImpact, -seedImpact, seedImpact), 3, 2.0, 2.0);
-
-        vec3 albedo = 3.0 * mix(color2, color1, smoothstep(0.4, 0.6, colorDecision1));
-
-        albedo = mix(color3, albedo, smoothSharpener(colorDecision2, colorSharpness));
-
-        float roughness = 0.4;
-        float metallic = 0.0;
-
-        // pbr accumulation
-        vec3 viewRayW = normalize(cameraPosition - vPositionW);
-        for (int i = 0; i < nbStars; i++) {
-            vec3 L = normalize(star_positions[i] - vPositionW);
-
-            Lo += calculateLight(albedo, normalW, roughness, metallic, L, viewRayW, star_colors[i]);
-        }
+        Lo += calculateLight(albedo, normalW, roughness, metallic, L, viewRayW, star_colors[i]);
     }
 
     gl_FragColor = vec4(Lo, 1.0);
