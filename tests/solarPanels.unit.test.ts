1ca963bd46e9e326a1fb177185688830409b9396
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-06-07 17:08:45 +0200

    added solar panels math and tests

---
+++ tests/solarPanels.unit.test.ts
@@ -0,0 +1,32 @@
+//  This file is part of Cosmos Journeyer
+//
+//  Copyright (C) 2024 Barthélemy Paléologue <barth.paleologue@cosmosjourneyer.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import { getSolarPanelSurfaceFromEnergyRequirement } from "../src/ts/utils/solarPanels";
+
+test("solarPanelSurfaceCalculation", () => {
+    // test with ISS data
+    const efficiency = 0.07;
+    const sunTemperature = 5778;
+    const sunExposure = 0.5;
+    const distanceToSun = 1.496e11;
+    const sunRadius = 6.9634e8;
+    const energyRequirement = 120000;
+
+    const solarPanelSurface = getSolarPanelSurfaceFromEnergyRequirement(efficiency, distanceToSun, sunTemperature, sunRadius, energyRequirement, sunExposure);
+    expect(solarPanelSurface).toBeGreaterThanOrEqual(2400);
+    expect(solarPanelSurface).toBeLessThanOrEqual(2600);
+});
