713d2a1e89d63350d84ac4df13600fc512c72995
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2024-06-27 13:54:57 +0200

    license & tree shaking

--- src/ts/asteroidBelts/asteroidBelt.ts
+++ src/ts/asteroidBelts/asteroidBelt.ts
@@ -1,4 +1,25 @@
-import { Matrix, PhysicsAggregate, PhysicsBody, PhysicsMotionType, PhysicsShapeType, Quaternion, Scene, TransformNode, Vector3 } from "@babylonjs/core";
+//  This file is part of Cosmos Journeyer
+//
+//  Copyright (C) 2024 Barthélemy Paléologue <barth.paleologue@cosmosjourneyer.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import { TransformNode } from "@babylonjs/core/Meshes/transformNode";
+import { PhysicsAggregate } from "@babylonjs/core/Physics/v2/physicsAggregate";
+import { Scene } from "@babylonjs/core/scene";
+import { Matrix, Quaternion, Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { PhysicsShapeType } from "@babylonjs/core/Physics/v2/IPhysicsEnginePlugin";
 import { IPatch } from "../planets/telluricPlanet/terrain/instancePatch/iPatch";
 import { Objects } from "../assets/objects";
 import { AsteroidPatch } from "./asteroidPatch";
