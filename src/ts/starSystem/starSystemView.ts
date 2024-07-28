fee8716b8ba1780d3747622d7dc353745bf5ff78
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-07-28 22:08:23 +0200

    Move near plane closer
    To enable 1st person views, a closer near plane will be needed.
    
    WebGPU gives again <3

--- src/ts/starSystem/starSystemView.ts
+++ src/ts/starSystem/starSystemView.ts
@@ -662,6 +662,7 @@ export class StarSystemView implements View {
             this.defaultControls = new DefaultControls(this.scene);
             this.defaultControls.speed = 0.2 * Settings.EARTH_RADIUS;
             this.defaultControls.getCameras().forEach((camera) => (camera.maxZ = maxZ));
+            this.defaultControls.getCameras().forEach((camera) => (camera.minZ = 0.1));
         }
 
         const spaceshipSerialized = this.player.serializedSpaceships.shift();
@@ -673,6 +674,7 @@ export class StarSystemView implements View {
         if (this.spaceshipControls === null) {
             this.spaceshipControls = new ShipControls(spaceship, this.scene);
             this.spaceshipControls.getCameras().forEach((camera) => (camera.maxZ = maxZ));
+            this.spaceshipControls.getCameras().forEach((camera) => (camera.minZ = 0.1));
         } else {
             const oldSpaceship = this.spaceshipControls.getSpaceship();
             this.spaceshipControls.reset();
@@ -684,6 +686,7 @@ export class StarSystemView implements View {
             this.characterControls = new CharacterControls(this.scene);
             this.characterControls.getTransform().setEnabled(false);
             this.characterControls.getCameras().forEach((camera) => (camera.maxZ = maxZ));
+            this.characterControls.getCameras().forEach((camera) => (camera.minZ = 0.1));
         }
 
         this.scene.setActiveControls(this.spaceshipControls);
