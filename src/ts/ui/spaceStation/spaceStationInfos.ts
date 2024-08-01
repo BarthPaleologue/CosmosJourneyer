7e027f072917f71fe2af740e3c2ee2ca700d1aed
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2024-08-01 14:39:50 +0200

    pie chart with a legend
    and better colors as well

--- src/ts/ui/spaceStation/spaceStationInfos.ts
+++ src/ts/ui/spaceStation/spaceStationInfos.ts
@@ -4,39 +4,26 @@ import { CropType, cropTypeToString } from "../../utils/agriculture";
 import * as d3 from "d3";
 
 export function generateInfoHTML(model: SpaceStationModel): string {
-
-
     const agricultureMix = model.agricultureMix;
 
     const pieGenerator = d3.pie<[number, CropType]>().value(([proportion, _]) => proportion);
-    
+
     const parsedData = pieGenerator(agricultureMix);
 
     const arcGenerator = d3
-    .arc<d3.PieArcDatum<[number, CropType]>>()
-    .innerRadius(0)
-    .outerRadius(200)
+        .arc<d3.PieArcDatum<[number, CropType]>>()
+        .innerRadius(0)
+        .outerRadius(200)
 
     const colorGenerator = d3
-  .scaleSequential(d3.interpolateWarm)
-  .domain([0, agricultureMix.length]);
+        .scaleSequential(d3.interpolateViridis)
+        .domain([0, agricultureMix.length]);
 
-  const arcs = parsedData.map((d, i) => ({
-    d: d,
-    path: arcGenerator(d),
-    data: d.data,
-    color: colorGenerator(i),
-  }))
-    .map(({ d, path, data, color }) => `
-    <g>
-        <path d="${path}" fill="${color}"></path>
-        <text transform="translate(${arcGenerator.centroid(d)})" text-anchor="middle">
-            ${cropTypeToString(data[1])}
-        </text>
-    </g>
-    `).join("")  
-
-
+    const arcs = parsedData.map((d, i) => ({
+        path: arcGenerator(d),
+        data: d.data,
+        color: colorGenerator(i),
+    }));
 
     return `
         <h2>General information</h2>
@@ -47,10 +34,16 @@ export function generateInfoHTML(model: SpaceStationModel): string {
 
         <h2>Agriculture mix</h2>
 
-        <svg viewBox="-250 -250 500 500">
-            ${arcs}
-        </svg>
+        <svg viewBox="-250 -250 700 500">
+            ${arcs.map(({ path, data, color }, i) => `
+            <g>
+                <path d="${path}" fill="${color}" class="pie-slice" title="${cropTypeToString(data[1])}"></path>
+            </g>
 
-        
-    `
+            <rect x="250" y="${(i - arcs.length / 2) * 40}" width="30" height="30" fill="${color}"></rect>
+            <text x="290" y="${(i - arcs.length / 2) * 40 + 24}" font-size="24">${cropTypeToString(data[1])}</text>
+
+            `).join("")}
+        </svg>
+    `;
 }
