54b8eba4c2cec95e44817ae5b817f8bc7b23036d
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2024-08-02 17:01:13 +0200

    precision of screenshot is always 1

--- src/ts/cosmosJourneyer.ts
+++ src/ts/cosmosJourneyer.ts
@@ -309,12 +309,11 @@ export class CosmosJourneyer {
 
     /**
      * Takes a screenshot of the current scene. By default, the screenshot is taken at a 4x the resolution of the canvas
-     * @param precision The resolution multiplier of the screenshot
      */
-    public takeScreenshot(precision = 4): void {
+    public takeScreenshot(): void {
         const camera = this.activeView.getMainScene().activeCamera;
         if (camera === null) throw new Error("Cannot take screenshot: camera is null");
-        Tools.CreateScreenshot(this.engine, camera, { precision: precision });
+        Tools.CreateScreenshot(this.engine, camera, { precision:1 });
     }
 
     public takeVideoCapture(): void {
