e0a0c77c24c11232dc2517672b80d24bbadcc7b8
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2024-08-07 14:50:10 +0200

    added basic powerplay data
    factions are distributed across 2 axis with some perlin noise field

---
+++ src/ts/powerplay/powerplay.ts
@@ -0,0 +1,30 @@
+import { seededSquirrelNoise } from "squirrel-noise";
+import { Settings } from "../settings";
+import { makeNoise3D } from "fast-simplex-noise";
+import { SystemSeed } from "../utils/systemSeed";
+import { getStarGalacticCoordinates } from "../utils/getStarGalacticCoordinates";
+
+const materialistSpiritualistRng = seededSquirrelNoise(Settings.POWER_PLAY_SEED);
+let materialistSpiritualistSampleStep = 0;
+const materialistSpiritalistPerlin = makeNoise3D(() => {
+    return materialistSpiritualistRng(materialistSpiritualistSampleStep++);
+});
+
+export const MaterialistSpiritalistAxis = (x: number, y: number, z: number) => (materialistSpiritalistPerlin(x * 0.5, y * 0.5, z * 0.5)) * 0.5 + 0.5;
+
+const capitalistCommunistRng = seededSquirrelNoise(Settings.POWER_PLAY_SEED + 598);
+let capitalistCommunistSampleStep = 0;
+const capitalistCommunistPerlin = makeNoise3D(() => {
+    return capitalistCommunistRng(capitalistCommunistSampleStep++);
+});
+
+export const CapitalistCommunistAxis = (x: number, y: number, z: number) => (capitalistCommunistPerlin(x * 0.5, y * 0.5, z * 0.5)) * 0.5 + 0.5;
+
+export function getPowerPlayData(systemSeed: SystemSeed) {
+    const coords = getStarGalacticCoordinates(systemSeed);
+
+    return {
+        materialistSpiritualist: MaterialistSpiritalistAxis(coords.x, coords.y, coords.z),
+        capitalistCommunist: CapitalistCommunistAxis(coords.x, coords.y, coords.z)
+    }
+}
