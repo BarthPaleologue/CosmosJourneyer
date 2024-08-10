bce5913d193d630eff1b58b7981a25fcf9ea8212
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-08-10 21:24:44 +0200

    decrease faction frequency for a more coherent view

--- src/ts/powerplay/powerplay.ts
+++ src/ts/powerplay/powerplay.ts
@@ -10,7 +10,7 @@ const materialistSpiritalistPerlin = makeNoise3D(() => {
     return materialistSpiritualistRng(materialistSpiritualistSampleStep++);
 });
 
-export const MaterialistSpiritalistAxis = (x: number, y: number, z: number) => (materialistSpiritalistPerlin(x * 0.5, y * 0.5, z * 0.5)) * 0.5 + 0.5;
+export const MaterialistSpiritalistAxis = (x: number, y: number, z: number) => (materialistSpiritalistPerlin(x * 0.2, y * 0.2, z * 0.2)) * 0.5 + 0.5;
 
 const capitalistCommunistRng = seededSquirrelNoise(Settings.POWER_PLAY_SEED + 598);
 let capitalistCommunistSampleStep = 0;
@@ -18,7 +18,7 @@ const capitalistCommunistPerlin = makeNoise3D(() => {
     return capitalistCommunistRng(capitalistCommunistSampleStep++);
 });
 
-export const CapitalistCommunistAxis = (x: number, y: number, z: number) => (capitalistCommunistPerlin(x * 0.5, y * 0.5, z * 0.5)) * 0.5 + 0.5;
+export const CapitalistCommunistAxis = (x: number, y: number, z: number) => (capitalistCommunistPerlin(x * 0.2, y * 0.2, z * 0.2)) * 0.5 + 0.5;
 
 export function getPowerPlayData(systemSeed: SystemSeed) {
     const coords = getStarGalacticCoordinates(systemSeed);
