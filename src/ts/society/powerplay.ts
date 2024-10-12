ee3f7ee7723844bb9fb347ac34d557e3162cffd5
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-10-12 23:34:28 +0200

    fix typos

--- src/ts/society/powerplay.ts
+++ src/ts/society/powerplay.ts
@@ -7,11 +7,11 @@ import { getRngFromSeed } from "../utils/getRngFromSeed";
 
 const materialistSpiritualistRng = getRngFromSeed(Settings.POWER_PLAY_SEED);
 let materialistSpiritualistSampleStep = 0;
-const materialistSpiritalistPerlin = makeNoise3D(() => {
+const materialistSpiritualistPerlin = makeNoise3D(() => {
     return materialistSpiritualistRng(materialistSpiritualistSampleStep++);
 });
 
-export const MaterialistSpiritalistAxis = (x: number, y: number, z: number) => materialistSpiritalistPerlin(x * 0.2, y * 0.2, z * 0.2) * 0.5 + 0.5;
+export const MaterialistSpiritualistAxis = (x: number, y: number, z: number) => materialistSpiritualistPerlin(x * 0.2, y * 0.2, z * 0.2) * 0.5 + 0.5;
 
 const capitalistCommunistRng = getRngFromSeed(Settings.POWER_PLAY_SEED + 598);
 let capitalistCommunistSampleStep = 0;
@@ -25,7 +25,7 @@ export function getPowerPlayData(systemCoordinates: StarSystemCoordinates) {
     const coords = getStarGalacticPosition(systemCoordinates);
 
     return {
-        materialistSpiritualist: MaterialistSpiritalistAxis(coords.x, coords.y, coords.z),
+        materialistSpiritualist: MaterialistSpiritualistAxis(coords.x, coords.y, coords.z),
         capitalistCommunist: CapitalistCommunistAxis(coords.x, coords.y, coords.z)
     };
 }
