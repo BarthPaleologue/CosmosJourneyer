9fd7dbb514119a08a1cb6130cf34fd609dcc3ebf
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-01-31 10:54:13 +0100

    add small orbital simulator

---
+++ src/ts/playgrounds/orbitalDemo.ts
@@ -0,0 +1,130 @@
+import { AbstractEngine } from "@babylonjs/core/Engines/abstractEngine";
+import { Scene } from "@babylonjs/core/scene";
+import { DefaultControls } from "../defaultControls/defaultControls";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { Matrix, Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
+import { OrbitalObjectWrapper } from "../utils/orbitalObjectWrapper";
+import { OrbitalObject, OrbitalObjectType } from "../architecture/orbitalObject";
+import { Tools } from "@babylonjs/core/Misc/tools";
+import { OrbitRenderer } from "../orbit/orbitRenderer";
+import { AxisRenderer } from "../orbit/axisRenderer";
+import { getPointOnOrbit } from "../orbit/orbit";
+
+export function createOrbitalDemoScene(engine: AbstractEngine): Scene {
+    const scene = new Scene(engine);
+    scene.useRightHandedSystem = true;
+
+    const defaultControls = new DefaultControls(scene);
+
+    const camera = defaultControls.getActiveCamera();
+    camera.minZ = 0.1;
+    camera.attachControl();
+
+    scene.enableDepthRenderer(camera, false, true);
+
+    const hemi = new HemisphericLight("hemi", Vector3.Up(), scene);
+    hemi.intensity = 1.0;
+
+    const sun = new OrbitalObjectWrapper(MeshBuilder.CreateSphere("sun", { diameter: 1 }, scene), {
+        name: "Sun",
+        orbit: {
+            argumentOfPeriapsis: 0,
+            semiMajorAxis: 0,
+            initialMeanAnomaly: 0,
+            longitudeOfAscendingNode: 0,
+            inclination: 0,
+            eccentricity: 0,
+            p: 2
+        },
+        type: OrbitalObjectType.STAR,
+        seed: 0,
+        physics: {
+            axialTilt: 0,
+            mass: 1e12,
+            siderealDaySeconds: 0
+        }
+    });
+
+    const earth = new OrbitalObjectWrapper(MeshBuilder.CreateSphere("earth", { diameter: 0.5 }, scene), {
+        name: "Earth",
+        orbit: {
+            argumentOfPeriapsis: 0,
+            semiMajorAxis: 10,
+            initialMeanAnomaly: 0,
+            longitudeOfAscendingNode: 0,
+            inclination: 0,
+            eccentricity: 0.5,
+            p: 2
+        },
+        type: OrbitalObjectType.TELLURIC_PLANET,
+        seed: 0,
+        physics: {
+            axialTilt: Tools.ToRadians(23.5),
+            mass: 1e11,
+            siderealDaySeconds: 0
+        }
+    });
+
+    const moon = new OrbitalObjectWrapper(MeshBuilder.CreateSphere("moon", { diameter: 0.2 }, scene), {
+        name: "Moon",
+        orbit: {
+            argumentOfPeriapsis: 0,
+            semiMajorAxis: 3,
+            initialMeanAnomaly: 0,
+            longitudeOfAscendingNode: 0,
+            inclination: Tools.ToRadians(45),
+            eccentricity: 0.7,
+            p: 2
+        },
+        type: OrbitalObjectType.TELLURIC_SATELLITE,
+        seed: 0,
+        physics: {
+            axialTilt: 0,
+            mass: 1,
+            siderealDaySeconds: 0
+        }
+    });
+
+    const bodyToParents = new Map<OrbitalObject, OrbitalObject[]>();
+    bodyToParents.set(earth, [sun]);
+    bodyToParents.set(moon, [earth]);
+
+    const orbitRenderer = new OrbitRenderer();
+    orbitRenderer.setOrbitalObjects(bodyToParents, scene);
+    orbitRenderer.setVisibility(true);
+
+    const axisRenderer = new AxisRenderer();
+    axisRenderer.setVisibility(true);
+    axisRenderer.setOrbitalObjects([sun, earth, moon], scene);
+
+    const referencePlaneRotation = Matrix.Identity();
+
+    let elapsedSeconds = 0;
+
+    scene.onBeforeRenderObservable.add(() => {
+        const deltaSeconds = engine.getDeltaTime() / 1000;
+        elapsedSeconds += deltaSeconds;
+        defaultControls.update(deltaSeconds);
+
+        earth.getTransform().position = getPointOnOrbit(
+            sun.getTransform().position,
+            sun.model.physics.mass,
+            earth.model.orbit,
+            elapsedSeconds,
+            referencePlaneRotation
+        );
+
+        moon.getTransform().position = getPointOnOrbit(
+            earth.getTransform().position,
+            earth.model.physics.mass,
+            moon.model.orbit,
+            elapsedSeconds,
+            referencePlaneRotation
+        );
+
+        orbitRenderer.update(referencePlaneRotation);
+    });
+
+    return scene;
+}
---
+++ src/ts/utils/orbitalObjectWrapper.ts
@@ -0,0 +1,56 @@
+//  This file is part of Cosmos Journeyer
+//
+//  Copyright (C) 2024 Barthélemy Paléologue <barth.paleologue@cosmosjourneyer.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import { TransformNode } from "@babylonjs/core/Meshes/transformNode";
+import { OrbitalObject, OrbitalObjectModel } from "../architecture/orbitalObject";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { Axis, Space } from "@babylonjs/core/Maths/math.axis";
+
+export class OrbitalObjectWrapper<Model extends OrbitalObjectModel> implements OrbitalObject {
+    private _transform: TransformNode;
+    readonly model: Model;
+    private readonly boundingRadius: number;
+    constructor(transform: TransformNode, model: Model) {
+        this._transform = transform;
+        this.model = model;
+
+        const boundingVectors = this.getTransform().getHierarchyBoundingVectors();
+        this.boundingRadius = boundingVectors.max.subtract(boundingVectors.min).length() / 2;
+
+        this.getTransform().rotate(Axis.Z, this.model.orbit.inclination + this.model.physics.axialTilt, Space.WORLD);
+    }
+
+    getTransform(): TransformNode {
+        return this._transform;
+    }
+
+    getBoundingRadius(): number {
+        return this.boundingRadius;
+    }
+
+    getRotationAxis(): Vector3 {
+        return this.getTransform().up;
+    }
+
+    getTypeName(): string {
+        return "Wrapped Orbital Object";
+    }
+
+    dispose(): void {
+        this.getTransform().dispose();
+    }
+}
