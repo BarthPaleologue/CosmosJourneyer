ceb300af9ca86799a2020bc5c8dd9a1331916a56
Nakul <nakulverma.py@gmail.com>
2025-02-02 22:34:02 +0530

    All things are done only web serving remains

--- vite.config.ts
+++ vite.config.ts
@@ -28,118 +28,132 @@ const bannerText = `
 `;
 
 export default defineConfig({
-  base: "./",
-  build: {
-    outDir: "dist",
-    target: "es2022",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "src/ts/index.ts"),
-        alphaTestis: resolve(__dirname, "src/ts/alphaTestis.ts"),
-        blackHole: resolve(__dirname, "src/ts/blackHoleDemo.ts"),
-        playground: resolve(__dirname, "src/ts/playground.ts"),
-        xr: resolve(__dirname, "src/ts/xr.ts"),
-        spaceStationGenerator: resolve(__dirname, "src/ts/spaceStationGenerator.ts"),
-        debugAssets: resolve(__dirname, "src/ts/debugAssets.ts"),
-      },
-      output: {
-        entryFileNames: "[name].[hash].js",
-        assetFileNames: "[name].[hash][extname]",
-        format: "es",
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-          if (id.includes("some-heavy-module")) {
-            return "heavy";
-          }
-          return null; // Ensure function always returns something
+    base: "./",
+    build: {
+        outDir: "dist",
+        target: "es2022",
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "src/ts/index.ts"),
+                alphaTestis: resolve(__dirname, "src/ts/alphaTestis.ts"),
+                blackHole: resolve(__dirname, "src/ts/blackHoleDemo.ts"),
+                playground: resolve(__dirname, "src/ts/playground.ts"),
+                xr: resolve(__dirname, "src/ts/xr.ts"),
+                spaceStationGenerator: resolve(__dirname, "src/ts/spaceStationGenerator.ts"),
+                debugAssets: resolve(__dirname, "src/ts/debugAssets.ts")
+            },
+            output: {
+                entryFileNames: "[name].[hash].js",
+                assetFileNames: "[name].[hash][extname]",
+                format: "es",
+                manualChunks: (id) => {
+                    if (id.includes("node_modules")) {
+                        return "vendor";
+                    }
+                    if (id.includes("some-heavy-module")) {
+                        return "heavy";
+                    }
+                    return null; // Ensure function always returns something
+                }
+            }
         },
-      },
+        chunkSizeWarningLimit: 6000,
+        sourcemap: !isProduction,
+        minify: isProduction ? "esbuild" : false
     },
-    chunkSizeWarningLimit: 6000,
-    sourcemap: !isProduction,
-    minify: isProduction ? "esbuild" : false,
-  },
-  server: {
-    port: 8080,
-    open: false,
-    host: "localhost",
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "same-origin",
+    server: {
+        port: 8080,
+        open: false,
+        host: "localhost",
+        headers: {
+            "Cross-Origin-Opener-Policy": "same-origin",
+            "Cross-Origin-Embedder-Policy": "same-origin"
+        }
     },
-  },
-  worker: {
-    format: "es"
-  },
-  plugins: [
-    wasm(),
-    topLevelAwait(),
-    glsl(),
-    vitePluginBanner(bannerText),
-    createHtmlPlugin({
-      minify: isProduction,
-      pages: [
-        {
-          filename: "index.html",
-          template: "src/html/index.html",
-          injectOptions: { data: { title: "Cosmos Journeyer" } },
-        },
-        {
-          filename: "alphaTestis.html",
-          template: "src/html/index.html",
-          injectOptions: { data: { title: "Alpha Testis - Cosmos Journeyer" } },
-        },
-        {
-          filename: "blackhole.html",
-          template: "src/html/index.html",
-          injectOptions: { data: { title: "Black Hole - Cosmos Journeyer" } },
-        },
-        {
-          filename: "playground.html",
-          template: "src/html/emptyIndex.html",
-          injectOptions: { data: { title: "Playground - Cosmos Journeyer" } },
-        },
-        {
-          filename: "xr.html",
-          template: "src/html/emptyIndex.html",
-          injectOptions: { data: { title: "XR - Cosmos Journeyer" } },
-        },
-        {
-          filename: "spaceStationGenerator.html",
-          template: "src/html/emptyIndex.html",
-          injectOptions: { data: { title: "Space Station Generator - Cosmos Journeyer" } },
-        },
-        {
-          filename: "debugAssets.html",
-          template: "src/html/emptyIndex.html",
-          injectOptions: { data: { title: "Debug Assets - Cosmos Journeyer" } },
-        },
-      ],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
+    worker: {
+        format: "es"
     },
-    extensions: [".ts", ".js", ".json"],
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/styles/variables.scss";`,
-      },
+    plugins: [
+        wasm(),
+        topLevelAwait(),
+        glsl(),
+        vitePluginBanner(bannerText),
+        createHtmlPlugin({
+            minify: isProduction,
+            pages: [
+                {
+                    filename: "index.html",
+                    template: "src/html/index.html",
+                    injectOptions: { data: { title: "Cosmos Journeyer" } }
+                },
+                {
+                    filename: "alphaTestis.html",
+                    template: "src/html/index.html",
+                    injectOptions: { data: { title: "Alpha Testis - Cosmos Journeyer" } }
+                },
+                {
+                    filename: "blackhole.html",
+                    template: "src/html/index.html",
+                    injectOptions: { data: { title: "Black Hole - Cosmos Journeyer" } }
+                },
+                {
+                    filename: "playground.html",
+                    template: "src/html/emptyIndex.html",
+                    injectOptions: { data: { title: "Playground - Cosmos Journeyer" } }
+                },
+                {
+                    filename: "xr.html",
+                    template: "src/html/emptyIndex.html",
+                    injectOptions: { data: { title: "XR - Cosmos Journeyer" } }
+                },
+                {
+                    filename: "spaceStationGenerator.html",
+                    template: "src/html/emptyIndex.html",
+                    injectOptions: { data: { title: "Space Station Generator - Cosmos Journeyer" } }
+                },
+                {
+                    filename: "debugAssets.html",
+                    template: "src/html/emptyIndex.html",
+                    injectOptions: { data: { title: "Debug Assets - Cosmos Journeyer" } }
+                }
+            ]
+        })
+    ],
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src")
+        },
+        extensions: [".ts", ".js", ".json"]
     },
-  },
-  assetsInclude: [
-    "**/*.glb", "**/*.env", "**/*.babylon", "**/*.eot", "**/*.svg", "**/*.ttf", "**/*.woff", "**/*.woff2",
-    "**/*.png", "**/*.jpg", "**/*.gif", "**/*.webp", "**/*.obj", "**/*.mp3", "**/*.dds", "**/*.html",
-  ],
-  optimizeDeps: {
-    esbuildOptions: {
-        target: "esnext"
-      }
-  },
-  esbuild: { target: "esnext"},
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "@/styles/variables.scss";`
+            }
+        }
+    },
+    assetsInclude: [
+        "**/*.glb",
+        "**/*.env",
+        "**/*.babylon",
+        "**/*.eot",
+        "**/*.svg",
+        "**/*.ttf",
+        "**/*.woff",
+        "**/*.woff2",
+        "**/*.png",
+        "**/*.jpg",
+        "**/*.gif",
+        "**/*.webp",
+        "**/*.obj",
+        "**/*.mp3",
+        "**/*.dds",
+        "**/*.html"
+    ],
+    optimizeDeps: {
+        esbuildOptions: {
+            target: "esnext"
+        }
+    },
+    esbuild: { target: "esnext" }
 });
