951ca7b0c0e8ee0b7e2584340eefa72866e4f323
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2025-03-03 17:32:33 +0100

    Change banner format
    The main issue all along was actually that the banner plugin was injecting C-style comments in the css code artefacts.
    
    As CSS does not support this type of comments, it broke the next style rule which was supposed to load the font...

--- webpack.config.js
+++ webpack.config.js
@@ -32,24 +32,25 @@ const config = {
     plugins: [
         new webpack.BannerPlugin({
             raw: true,
-            banner: `
-//  This file is part of Cosmos Journeyer
-//
-//  Copyright (C) 2024 Barthélemy Paléologue <barth.paleologue@cosmosjourneyer.com>
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Affero General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Affero General Public License for more details.
-//
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-        `,
+            banner: `/*
+ *  This file is part of Cosmos Journeyer
+ *
+ *  Copyright (C) 2024 Barthélemy Paléologue <barth.paleologue@cosmosjourneyer.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+`,
             stage: webpack.Compilation.PROCESS_ASSETS_STAGE_REPORT
         }),
         new MiniCssExtractPlugin({
