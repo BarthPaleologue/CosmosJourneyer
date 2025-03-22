64b6202d53a45a7260e96281c17b83ad5a66f227
BarthPaleologue <BarthPaleologue@users.noreply.github.com>
2025-03-22 15:13:06 +0000

    Deploy preview for PR 342 🛫

---
+++ pr-preview/pr-342/77.40a9143e35653297.js
@@ -0,0 +1,4 @@
+"use strict";(globalThis.webpackChunkcosmos_journeyer=globalThis.webpackChunkcosmos_journeyer||[]).push([["77"],{95620:function(e,r,a){a.r(r),a.d(r,{passPixelShader:()=>i});var o=a(19015);let s="passPixelShader",l=`varying vec2 vUV;uniform sampler2D textureSampler;
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{gl_FragColor=texture2D(textureSampler,vUV);}`;o.v.ShadersStore[s]=l;let i={name:s,shader:l}}}]);
