7d040a04a83c715eb99c2069c382ddcd6bb9d353
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2025-04-27 13:38:14 +0200

    Fix cameras using disposed post-processes

--- src/ts/postProcesses/postProcessManager.ts
+++ src/ts/postProcesses/postProcessManager.ts
@@ -813,6 +813,9 @@ export class PostProcessManager {
      * The pipeline is not destroyed as it is always destroyed and recreated when the closest orbital object changes.
      */
     public reset() {
+        this.renderingPipelineManager.detachCamerasFromRenderPipeline(this.renderingPipeline.name, this.scene.cameras);
+        this.renderingPipelineManager.removePipeline(this.renderingPipeline.name);
+
         // disposing on every camera is necessary because BabylonJS only detaches the post-processes from a single camera at a time
         this.scene.cameras.forEach((camera) => {
             this.objectPostProcesses.forEach((postProcessList) => {
