902c274431f8b2b3de73cfcac7d20832705fb05a
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2025-05-13 17:40:02 +0200

    fix vitest path alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,14 @@
+import path from "path";
+
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
         environment: "jsdom",
         include: ["**/*.{test,spec}.ts"],
-        exclude: ["node_modules", "dist", ".git", "tests/e2e/**"]
+        exclude: ["node_modules", "dist", ".git", "tests/e2e/**"],
+        alias: {
+            "@": path.resolve(__dirname, "./src/ts")
+        }
     }
 });
