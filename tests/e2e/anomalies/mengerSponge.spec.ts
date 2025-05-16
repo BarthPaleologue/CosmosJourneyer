cb36dad5ad50af1cc7b99728d30f243eb52828d6
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2025-05-16 09:58:52 +0200

    step 16

--- tests/e2e/anomalies/mengerSponge.spec.ts
+++ tests/e2e/anomalies/mengerSponge.spec.ts
@@ -1,6 +1,6 @@
 import { test } from "@playwright/test";
 
-import { renderAndSnap } from "@/utils/renderSnap";
+import { renderAndSnap } from "../utils/renderSnap";
 
 test("The Menger Sponge playground renders correctly", async ({ page }) => {
     await renderAndSnap(page, {
