7e667a697cd7beca9e3aac88f3264436deedf9b3
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2025-05-25 12:29:08 +0200

    Fix linting

--- src/ts/playgrounds/rings.ts
+++ src/ts/playgrounds/rings.ts
@@ -15,7 +15,7 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import { Color3, MeshBuilder, PointLight, Vector3 } from "@babylonjs/core";
+import { MeshBuilder, PointLight, Vector3 } from "@babylonjs/core";
 import { AbstractEngine } from "@babylonjs/core/Engines/abstractEngine";
 import { Scene } from "@babylonjs/core/scene";
 
