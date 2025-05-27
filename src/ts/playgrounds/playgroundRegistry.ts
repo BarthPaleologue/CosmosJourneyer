3f6cf9d3c4e289c329c497e46d4f641c2bea6a88
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2025-05-27 22:50:58 +0200

    Bump playground requirement to have a WebGPU engine

--- src/ts/playgrounds/playgroundRegistry.ts
+++ src/ts/playgrounds/playgroundRegistry.ts
@@ -15,7 +15,7 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import { AbstractEngine, Scene } from "@babylonjs/core";
+import { Scene, WebGPUEngine } from "@babylonjs/core";
 
 import { createJuliaSetScene } from "./anomalies/juliaSet";
 import { createMandelboxScene } from "./anomalies/mandelbox";
@@ -52,7 +52,7 @@ import { createXrScene } from "./xr";
 export class PlaygroundRegistry {
     private readonly map: Map<
         string,
-        (engine: AbstractEngine, progressCallback: (progress: number, text: string) => void) => Promise<Scene>
+        (engine: WebGPUEngine, progressCallback: (progress: number, text: string) => void) => Promise<Scene>
     > = new Map([
         ["orbitalDemo", createOrbitalDemoScene],
         ["tunnel", createHyperspaceTunnelDemo],
@@ -89,7 +89,7 @@ export class PlaygroundRegistry {
     register(
         name: string,
         createScene: (
-            engine: AbstractEngine,
+            engine: WebGPUEngine,
             progressCallback: (progress: number, text: string) => void,
         ) => Promise<Scene>,
     ) {
@@ -98,7 +98,7 @@ export class PlaygroundRegistry {
 
     get(
         name: string,
-    ): (engine: AbstractEngine, progressCallback: (progress: number, text: string) => void) => Promise<Scene> {
+    ): (engine: WebGPUEngine, progressCallback: (progress: number, text: string) => void) => Promise<Scene> {
         return this.map.get(name) ?? createDefaultScene;
     }
 }
