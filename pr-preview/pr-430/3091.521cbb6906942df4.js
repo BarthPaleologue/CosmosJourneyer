d76eee7ce37311ad7c16ac383a4495c7e392c9b7
BarthPaleologue <BarthPaleologue@users.noreply.github.com>
2025-05-31 12:14:11 +0000

    Deploy preview for PR 430 🛫

---
+++ pr-preview/pr-430/3091.521cbb6906942df4.js
@@ -0,0 +1,23 @@
+"use strict";(globalThis.webpackChunkcosmos_journeyer=globalThis.webpackChunkcosmos_journeyer||[]).push([["3091"],{9597:function(e,o,r){r.r(o),r.d(o,{glowMapMergePixelShader:()=>f});var a=r(80709);let l="glowMapMergePixelShader",s=`varying vec2 vUV;uniform sampler2D textureSampler;
+#ifdef EMISSIVE
+uniform sampler2D textureSampler2;
+#endif
+uniform float offset;
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) {
+#define CUSTOM_FRAGMENT_MAIN_BEGIN
+vec4 baseColor=texture2D(textureSampler,vUV);
+#ifdef EMISSIVE
+baseColor+=texture2D(textureSampler2,vUV);baseColor*=offset;
+#else
+baseColor.a=abs(offset-baseColor.a);
+#ifdef STROKE
+float alpha=smoothstep(.0,.1,baseColor.a);baseColor.a=alpha;baseColor.rgb=baseColor.rgb*alpha;
+#endif
+#endif
+#if LDR
+baseColor=clamp(baseColor,0.,1.0);
+#endif
+gl_FragColor=baseColor;
+#define CUSTOM_FRAGMENT_MAIN_END
+}`;a.v.ShadersStore[l]||(a.v.ShadersStore[l]=s);let f={name:l,shader:s}}}]);
