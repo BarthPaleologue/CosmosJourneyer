8874d3f37d12a3b54923f12c005d5c38b3a0739c
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2025-06-07 12:36:43 +0200

    Move to physics atmosphere folder and rename to rayleigh scattering

--- src/ts/utils/physics/atmosphere/rayleighScattering.ts
+++ src/ts/utils/physics/atmosphere/rayleighScattering.ts
@@ -15,7 +15,7 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import { DeepReadonly } from "./types";
+import { DeepReadonly } from "../../types";
 
 /**
  * Boltzmann constant, J K⁻¹  (CODATA 2019)
