194d9fda787480b33266a85b403a9317f1372d7f
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2025-06-09 12:40:13 +0200

    Extract sun model to separate file

--- src/ts/backend/universe/customSystems/sol/sol.ts
+++ src/ts/backend/universe/customSystems/sol/sol.ts
@@ -20,7 +20,6 @@ import { Tools } from "@babylonjs/core/Misc/tools";
 
 import { GasPlanetModel } from "@/backend/universe/orbitalObjects/gasPlanetModel";
 import { OrbitalObjectType } from "@/backend/universe/orbitalObjects/orbitalObjectType";
-import { StarModel } from "@/backend/universe/orbitalObjects/stellarObjects/starModel";
 import { TelluricPlanetModel } from "@/backend/universe/orbitalObjects/telluricPlanetModel";
 import { TelluricSatelliteModel } from "@/backend/universe/orbitalObjects/telluricSatelliteModel";
 import { StarSystemModel } from "@/backend/universe/starSystemModel";
@@ -31,30 +30,10 @@ import { Settings } from "@/settings";
 
 import { getJupiterModel } from "./jupiter";
 import { getSaturnModel } from "./saturn";
+import { getSunModel } from "./sun";
 
 export function getSolSystemModel(): StarSystemModel {
-    const sun: StarModel = {
-        id: "sun",
-        name: "Sun",
-        type: OrbitalObjectType.STAR,
-        radius: 695_508e3,
-        mass: 1.989e30,
-        blackBodyTemperature: 5778,
-        axialTilt: Tools.ToRadians(7.25),
-        siderealDaySeconds: 60 * 60 * 24 * 25.67,
-        orbit: {
-            parentIds: [],
-            semiMajorAxis: 0,
-            eccentricity: 0,
-            p: 2,
-            inclination: 0,
-            longitudeOfAscendingNode: 0,
-            argumentOfPeriapsis: 0,
-            initialMeanAnomaly: 0,
-        },
-        rings: null,
-        seed: 0,
-    };
+    const sun = getSunModel();
 
     const mercury: TelluricPlanetModel = {
         id: "mercury",
@@ -298,9 +277,9 @@ export function getSolSystemModel(): StarSystemModel {
         seed: 0,
     };
 
-    const jupiter: GasPlanetModel = getJupiterModel([sun.id]);
+    const jupiter = getJupiterModel([sun.id]);
 
-    const saturn: GasPlanetModel = getSaturnModel([sun.id]);
+    const saturn = getSaturnModel([sun.id]);
 
     const uranus: GasPlanetModel = {
         id: "uranus",
