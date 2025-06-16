b98a09229285993c0865730f69d2ea887501763d
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-06-16 20:00:03 +0200

    Add basic terrain model definition

---
+++ src/ts/backend/universe/orbitalObjects/terrainModel.ts
@@ -0,0 +1,41 @@
+//  This file is part of Cosmos Journeyer
+//
+//  Copyright (C) 2024 Barthélemy Paléologue <barth.paleologue@cosmosjourneyer.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import { z } from "zod";
+
+export const ProceduralTerrainModelSchema = z.object({
+    type: z.literal("procedural"),
+});
+
+export type ProceduralTerrainModel = z.infer<typeof ProceduralTerrainModelSchema>;
+
+export const CustomTerrainModelSchema = z.object({
+    type: z.literal("custom"),
+    heightRange: z.object({
+        min: z.number(),
+        max: z.number(),
+    }),
+});
+
+export type CustomTerrainModel = z.infer<typeof CustomTerrainModelSchema>;
+
+export const TerrainModelSchema = z.discriminatedUnion("type", [
+    ProceduralTerrainModelSchema,
+    CustomTerrainModelSchema,
+]);
+
+export type TerrainModel = z.infer<typeof TerrainModelSchema>;
