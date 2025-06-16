1caf85ca66da8228c78126855063298921d6ccf1
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-06-16 16:12:56 +0200

    Fix atmosphere shader mistakes

--- src/shaders/atmosphericScatteringFragment.glsl
+++ src/shaders/atmosphericScatteringFragment.glsl
@@ -43,7 +43,7 @@ uniform sampler2D depthSampler;// the depth map of the camera
 
 // based on https://www.youtube.com/watch?v=DxfEbulyFcY by Sebastian Lague
 vec3 densityAtPoint(vec3 densitySamplePoint) {
-    float heightAboveSurface = length(densitySamplePoint - object_position) - object_radius;// actual height above surface
+    float heightAboveSurface = max(0.0, length(densitySamplePoint - object_position) - object_radius);// actual height above surface
 
     // rayleigh and mie
     vec3 density = vec3(exp(-heightAboveSurface / vec2(atmosphere_rayleighHeight, atmosphere_mieHeight)), 0.0);
@@ -57,7 +57,7 @@ vec3 densityAtPoint(vec3 densitySamplePoint) {
 
 vec3 opticalDepth(vec3 rayOrigin, vec3 rayDir, float rayLength) {
 
-    float stepSize = rayLength / (float(OPTICAL_DEPTH_POINTS) - 1.0);// ray length between sample points
+    float stepSize = rayLength / float(OPTICAL_DEPTH_POINTS);// ray length between sample points
 
     vec3 densitySamplePoint = rayOrigin;// that's where we start
     vec3 accumulatedOpticalDepth = vec3(0.0);
@@ -79,7 +79,7 @@ vec3 calculateLight(vec3 rayOrigin, vec3 starPosition, vec3 rayDir, float rayLen
 
     vec3 starDir = normalize(starPosition - object_position);// direction to the light source
 
-    float stepSize = rayLength / (float(POINTS_FROM_CAMERA) - 1.0);// the ray length between sample points
+    float stepSize = rayLength / float(POINTS_FROM_CAMERA);// the ray length between sample points
 
     vec3 inScatteredRayleigh = vec3(0.0);
     vec3 inScatteredMie = vec3(0.0);
