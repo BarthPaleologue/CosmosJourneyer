e1a065729e3d3e2b4775582032329f74b9bfe697
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-06-21 19:40:06 +0200

    Enforce row vertex count power of two
    GPUs do like power of twos

--- src/ts/utils/types.ts
+++ src/ts/utils/types.ts
@@ -59,3 +59,36 @@ type BuildTuple<T, N extends number, Acc extends T[] = []> = Acc["length"] exten
     : BuildTuple<T, N, [T, ...Acc]>; // Otherwise, prepend T and recurse
 
 export type FixedLengthArray<T, N extends number> = BuildTuple<T, N>;
+
+export type PowerOfTwo =
+    | 1
+    | 2
+    | 4
+    | 8
+    | 16
+    | 32
+    | 64
+    | 128
+    | 256
+    | 512
+    | 1_024
+    | 2_048
+    | 4_096
+    | 8_192
+    | 16_384
+    | 32_768
+    | 65_536
+    | 131_072
+    | 262_144
+    | 524_288
+    | 1_048_576
+    | 2_097_152
+    | 4_194_304
+    | 8_388_608
+    | 16_777_216
+    | 33_554_432
+    | 67_108_864
+    | 134_217_728
+    | 268_435_456
+    | 536_870_912
+    | 1_073_741_824;
