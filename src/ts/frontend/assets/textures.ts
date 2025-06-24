a1ed533260d362294b7d798a8ac0a97288ea0966
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-06-24 17:21:11 +0200

    Move LRUMap (and ItemPool and PriorityQueue) to a new utils/dataStructures folder

--- src/ts/frontend/assets/textures.ts
+++ src/ts/frontend/assets/textures.ts
@@ -27,7 +27,7 @@ import { RingsProceduralPatternLut } from "@/frontend/postProcesses/rings/ringsP
 import { TelluricPlanetMaterialLut } from "@/frontend/universe/planets/telluricPlanet/telluricPlanetMaterialLut";
 import { StarMaterialLut } from "@/frontend/universe/stellarObjects/star/starMaterialLut";
 
-import { ItemPool } from "@/utils/itemPool";
+import { ItemPool } from "@/utils/dataStructures/itemPool";
 
 import { HeightMap1x1, HeightMap2x4 } from "./heightMaps";
 import { LandingPadTexturePool } from "./landingPadTexturePool";
