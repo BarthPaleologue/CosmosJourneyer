47e0d919806b1cecbe84ddbadf612c288846bffd
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-06-24 18:25:46 +0200

    Create Earth playground

--- src/ts/playgrounds/playgroundRegistry.ts
+++ src/ts/playgrounds/playgroundRegistry.ts
@@ -35,6 +35,7 @@ import { createHyperspaceTunnelDemo } from "./hyperspaceTunnel";
 import { createOrbitalDemoScene } from "./orbitalDemo";
 import { createRingsScene } from "./rings";
 import { createSaveLoadingPanelContentScene } from "./saveLoadingPanelContent";
+import { createEarthScene } from "./sol/earth";
 import { createJupiterScene } from "./sol/jupiter";
 import { createSaturnScene } from "./sol/saturn";
 import { createSolScene } from "./sol/sol";
@@ -88,6 +89,7 @@ export class PlaygroundRegistry {
         ["sun", createSunScene],
         ["terrain", createTerrainScene],
         ["sphericalTerrain", createSphericalHeightFieldTerrain],
+        ["earth", createEarthScene],
     ]);
 
     register(
