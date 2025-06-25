8c610579eb1d1e831ec364a9fac11c04d5062340
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-06-25 18:00:34 +0200

    Add E2E test to avoid regression on the billboarding issue

--- tests/e2e/starMap.spec.ts
+++ tests/e2e/starMap.spec.ts
@@ -10,3 +10,24 @@ test("The star map playground renders correctly", async ({ page }) => {
         urlParams: { freeze: 3 },
     });
 });
+
+test("The star map playground renders correctly when targeting another system", async ({ page }) => {
+    await renderAndSnap(page, {
+        shotName: "baselineOtherSystem",
+        scene: "starMap",
+        flagToWait: "frozen",
+        urlParams: {
+            freeze: 3,
+            systemCoordinates: encodeURIComponent(
+                JSON.stringify({
+                    localX: -0.1547746381977455,
+                    localY: -0.4600146743282744,
+                    localZ: 0.35319629770386796,
+                    starSectorX: 7,
+                    starSectorY: -9,
+                    starSectorZ: 9,
+                }),
+            ),
+        },
+    });
+});
