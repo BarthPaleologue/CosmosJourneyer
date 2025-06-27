24fbd56529ccd55056c21059724c27c05dc34060
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-06-27 17:07:23 +0200

    Load mercury textures

--- src/ts/frontend/assets/textures/heightmaps/mercury.ts
+++ src/ts/frontend/assets/textures/heightmaps/mercury.ts
@@ -18,30 +18,17 @@
 import type { Scene } from "@babylonjs/core/scene";
 
 import type { ILoadingProgressMonitor } from "../../loadingProgressMonitor";
-import { loadEarthHeightMap1x1, loadEarthHeightMap2x4 } from "./earth";
-import { loadMarsHeightMap1x1, loadMarsHeightMapHighResolution } from "./mars";
-import { loadMercuryHeightMap1x1 } from "./mercury";
-import type { HeightMap1x1, HeightMap2x4 } from "./types";
+import { loadTextureAsync } from "../utils";
+import { type HeightMap1x1 } from "./types";
 
-export type HeightMaps = {
-    mercury1x1: Readonly<HeightMap1x1>;
-    earth1x1: Readonly<HeightMap1x1>;
-    earth2x4: Readonly<HeightMap2x4>;
-    mars1x1: Readonly<HeightMap1x1>;
-    mars2x4: Readonly<HeightMap2x4>;
-};
+import mercuryHeightMap1x1 from "@assets/sol/textures/mercuryHeightMap8k.png";
 
-export async function loadHeightMaps(
+export async function loadMercuryHeightMap1x1(
     scene: Scene,
     progressMonitor: ILoadingProgressMonitor | null,
-): Promise<HeightMaps> {
-    const heightMaps: HeightMaps = {
-        mercury1x1: await loadMercuryHeightMap1x1(scene, progressMonitor),
-        earth1x1: await loadEarthHeightMap1x1(scene, progressMonitor),
-        earth2x4: await loadEarthHeightMap2x4(scene, progressMonitor),
-        mars1x1: await loadMarsHeightMap1x1(scene, progressMonitor),
-        mars2x4: await loadMarsHeightMapHighResolution(scene, progressMonitor),
+): Promise<HeightMap1x1> {
+    return {
+        type: "1x1",
+        texture: await loadTextureAsync("mercuryHeightMap1x1", mercuryHeightMap1x1, scene, progressMonitor),
     };
-
-    return heightMaps;
 }
