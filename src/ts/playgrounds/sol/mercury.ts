79ed7a82d4e7bceb8265e22820bbaab670ff02c2
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-07-08 18:34:30 +0200

    Add support for tiled custom planet color maps

--- src/ts/playgrounds/sol/mercury.ts
+++ src/ts/playgrounds/sol/mercury.ts
@@ -81,7 +81,11 @@ export async function createMercuryScene(
     const albedoMap = new Texture(mercuryColorMapPath, scene);
     const normalMap = new Texture(mercuryNormalMapPath, scene);
 
-    const material = new CustomPlanetMaterial(albedoMap, normalMap, scene);
+    const material = new CustomPlanetMaterial(
+        { type: "texture_2d", texture: albedoMap },
+        { type: "texture_2d", texture: normalMap },
+        scene,
+    );
 
     const terrainModel: TerrainModel = {
         type: "custom",
