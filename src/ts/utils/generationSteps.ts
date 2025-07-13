2e712b6b2270a989c2ba6a93b0a9cfd883acf9ac
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-07-13 17:54:25 +0200

    Add randomness to mie scattering for sterile worlds

--- src/ts/utils/generationSteps.ts
+++ src/ts/utils/generationSteps.ts
@@ -15,25 +15,34 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-export const enum GenerationSteps {
-    AXIAL_TILT = 100,
-    ORBIT = 200,
-    RADIUS = 1000,
-    ORBITAL_PLANE_ALIGNMENT = 1600,
+export const GenerationSteps = {
+    AXIAL_TILT: 100,
+    ORBIT: 200,
+    RADIUS: 1000,
+    ORBITAL_PLANE_ALIGNMENT: 1600,
 
-    RINGS = 1200,
+    RINGS: 1200,
 
-    POWER = 300,
-    ACCENT_COLOR = 400,
+    POWER: 300,
+    ACCENT_COLOR: 400,
 
-    TEMPERATURE = 1100,
-    STELLAR_TYPE = 1900,
+    TEMPERATURE: 1100,
+    STELLAR_TYPE: 1900,
 
-    PRESSURE = 1800,
-    WATER_AMOUNT = 1700,
-    TERRAIN = 1500,
+    PRESSURE: 1800,
+    WATER_AMOUNT: 1700,
+    TERRAIN: 1500,
 
-    DIPOLE_TILT = 1300,
+    DIPOLE_TILT: 1300,
 
-    SIDEREAL_DAY_SECONDS = 2500,
-}
+    SIDEREAL_DAY_SECONDS: 2500,
+
+    ATMOSPHERE: {
+        AEROSOLS: {
+            TAU_550: 3000,
+            SETTLING_COEFFICIENT: 3100,
+            PARTICLE_RADIUS: 3200,
+            ANGSTROM_EXPONENT: 3300,
+        },
+    },
+} as const;
