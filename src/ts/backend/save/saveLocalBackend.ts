49a51819ffb30a1dff2de5f52d708c99272303be
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-07-18 17:22:20 +0200

    WIIIP

--- src/ts/backend/save/saveLocalBackend.ts
+++ src/ts/backend/save/saveLocalBackend.ts
@@ -15,99 +15,29 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import { type StarSystemDatabase } from "@/backend/universe/starSystemDatabase";
-
-import { jsonSafeParse } from "@/utils/json";
-import { err, ok, type Result } from "@/utils/types";
-
-import { parseSaveArray, SavesSchema, type CmdrSaves, type Save } from "./saveFileData";
-import { SaveLoadingErrorType, type SaveLoadingError } from "./saveLoadingError";
 import { type SaveBackend } from "./saveManager";
 
 export class SaveLocalBackend implements SaveBackend {
     public static readonly SAVES_KEY = "saves";
     public static readonly BACKUP_SAVE_KEY = "backupSaves";
 
-    public write(saves: Record<string, CmdrSaves>): boolean {
-        localStorage.setItem(SaveLocalBackend.SAVES_KEY, JSON.stringify(saves));
-
+    public write(content: string): boolean {
+        localStorage.setItem(SaveLocalBackend.SAVES_KEY, content);
         return true;
     }
 
-    public read(starSystemDatabase: StarSystemDatabase): Promise<Result<Record<string, CmdrSaves>, SaveLoadingError>> {
+    public writeBackup(content: string): boolean {
+        localStorage.setItem(SaveLocalBackend.BACKUP_SAVE_KEY, content);
+        return true;
+    }
+
+    public read(): Promise<string | null> {
         const rawSaves = localStorage.getItem(SaveLocalBackend.SAVES_KEY);
-        const rawBackupSaves = localStorage.getItem(SaveLocalBackend.BACKUP_SAVE_KEY);
-        if (rawSaves === null && rawBackupSaves === null) {
-            return Promise.resolve(ok({}));
-        }
+        return Promise.resolve(rawSaves);
+    }
 
-        const parsedSaves = jsonSafeParse(rawSaves ?? "{}");
-        if (parsedSaves === null) {
-            return Promise.resolve(err({ type: SaveLoadingErrorType.INVALID_JSON }));
-        }
-
-        const parsedBackupSaves = jsonSafeParse(rawBackupSaves ?? "{}");
-        if (parsedBackupSaves === null) {
-            return Promise.resolve(err({ type: SaveLoadingErrorType.INVALID_JSON }));
-        }
-
-        const savesResult = SavesSchema.safeParse(parsedSaves);
-        if (!savesResult.success) {
-            console.error(savesResult.error);
-            return Promise.resolve(
-                err({ type: SaveLoadingErrorType.INVALID_STORAGE_FORMAT, content: savesResult.error }),
-            );
-        }
-
-        const backupSavesResult = SavesSchema.safeParse(parsedBackupSaves);
-        if (!backupSavesResult.success) {
-            console.error(backupSavesResult.error);
-            return Promise.resolve(
-                err({ type: SaveLoadingErrorType.INVALID_STORAGE_FORMAT, content: backupSavesResult.error }),
-            );
-        }
-
-        const saves = savesResult.data;
-        const backupSaves = backupSavesResult.data;
-
-        const allSaves = {
-            ...saves,
-            ...backupSaves,
-        };
-
-        const correctSaves: Record<string, { manual: Save[]; auto: Save[] }> = {};
-        const corruptedSaves: Record<string, { manual: unknown[]; auto: unknown[] }> = {};
-
-        // filter saves
-        for (const [cmdrUuid, cmdrSaves] of Object.entries(allSaves)) {
-            const parsedManualSaves = parseSaveArray(cmdrSaves.manual, starSystemDatabase);
-            const parsedAutoSaves = parseSaveArray(cmdrSaves.auto, starSystemDatabase);
-
-            correctSaves[cmdrUuid] = {
-                manual: parsedManualSaves.validSaves,
-                auto: parsedAutoSaves.validSaves,
-            };
-
-            if (parsedManualSaves.invalidSaves.length > 0 || parsedAutoSaves.invalidSaves.length > 0) {
-                corruptedSaves[cmdrUuid] = {
-                    manual: parsedManualSaves.invalidSaves.map((save) => {
-                        console.error("Corrupted manual save:", save.save, save.error);
-                        return save.save;
-                    }),
-                    auto: parsedAutoSaves.invalidSaves.map((save) => {
-                        console.error("Corrupted auto save:", save.save, save.error);
-                        return save.save;
-                    }),
-                };
-            }
-        }
-
-        localStorage.setItem(SaveLocalBackend.BACKUP_SAVE_KEY, JSON.stringify(corruptedSaves));
-
-        if (Object.keys(corruptedSaves).length > 0) {
-            console.warn("Some save files could not be validated! Check the console for more information.");
-        }
-
-        return Promise.resolve(ok(correctSaves));
+    public readBackup(): Promise<string | null> {
+        const backupSaves = localStorage.getItem(SaveLocalBackend.BACKUP_SAVE_KEY);
+        return Promise.resolve(backupSaves);
     }
 }
--- src/ts/backend/save/saveManager.ts
+++ src/ts/backend/save/saveManager.ts
@@ -17,12 +17,13 @@
 
 import { type StarSystemDatabase } from "@/backend/universe/starSystemDatabase";
 
-import { ok, type Result } from "@/utils/types";
+import { jsonSafeParse } from "@/utils/json";
+import { err, ok, type Result } from "@/utils/types";
 
 import { Settings } from "@/settings";
 
-import { type CmdrSaves, type Save } from "./saveFileData";
-import { type SaveLoadingError } from "./saveLoadingError";
+import { parseSaveArray, SavesSchema, type CmdrSaves, type Save } from "./saveFileData";
+import { SaveLoadingErrorType, type SaveLoadingError } from "./saveLoadingError";
 
 /**
  * Interface defining the storage backend for save data.
@@ -33,13 +34,17 @@ export interface SaveBackend {
      * @param saves - Record of commander saves, keyed by commander string
      * @returns Boolean indicating success or failure of the write operation
      */
-    write(saves: Record<string, CmdrSaves>): boolean;
+    write(content: string): boolean;
+
+    writeBackup(content: string): boolean;
 
     /**
      * Reads save data from the storage backend.
      * @returns Result containing either the loaded saves or an error
      */
-    read(starSystemDatabase: StarSystemDatabase): Promise<Result<Record<string, CmdrSaves>, SaveLoadingError>>;
+    read(): Promise<string | null>;
+
+    readBackup(): Promise<string | null>;
 }
 
 export interface ISaveManager {
@@ -94,13 +99,81 @@ export class SaveManager implements ISaveManager {
         backend: SaveBackend,
         starSystemDatabase: StarSystemDatabase,
     ): Promise<Result<SaveManager, SaveLoadingError>> {
-        const saveResult = await backend.read(starSystemDatabase);
-        if (!saveResult.success) {
-            return saveResult;
+        const rawSaves = await backend.read();
+        const rawBackupSaves = await backend.readBackup();
+        if (rawSaves === null && rawBackupSaves === null) {
+            return Promise.resolve(ok({}));
+        }
+
+        const parsedSaves = jsonSafeParse(rawSaves ?? "{}");
+        if (parsedSaves === null) {
+            return Promise.resolve(err({ type: SaveLoadingErrorType.INVALID_JSON }));
+        }
+
+        const parsedBackupSaves = jsonSafeParse(rawBackupSaves ?? "{}");
+        if (parsedBackupSaves === null) {
+            return Promise.resolve(err({ type: SaveLoadingErrorType.INVALID_JSON }));
+        }
+
+        const savesResult = SavesSchema.safeParse(parsedSaves);
+        if (!savesResult.success) {
+            console.error(savesResult.error);
+            return Promise.resolve(
+                err({ type: SaveLoadingErrorType.INVALID_STORAGE_FORMAT, content: savesResult.error }),
+            );
+        }
+
+        const backupSavesResult = SavesSchema.safeParse(parsedBackupSaves);
+        if (!backupSavesResult.success) {
+            console.error(backupSavesResult.error);
+            return Promise.resolve(
+                err({ type: SaveLoadingErrorType.INVALID_STORAGE_FORMAT, content: backupSavesResult.error }),
+            );
+        }
+
+        const saves = savesResult.data;
+        const backupSaves = backupSavesResult.data;
+
+        const allSaves = {
+            ...saves,
+            ...backupSaves,
+        };
+
+        const correctSaves: Record<string, { manual: Save[]; auto: Save[] }> = {};
+        const corruptedSaves: Record<string, { manual: unknown[]; auto: unknown[] }> = {};
+
+        // filter saves
+        for (const [cmdrUuid, cmdrSaves] of Object.entries(allSaves)) {
+            const parsedManualSaves = parseSaveArray(cmdrSaves.manual, starSystemDatabase);
+            const parsedAutoSaves = parseSaveArray(cmdrSaves.auto, starSystemDatabase);
+
+            correctSaves[cmdrUuid] = {
+                manual: parsedManualSaves.validSaves,
+                auto: parsedAutoSaves.validSaves,
+            };
+
+            if (parsedManualSaves.invalidSaves.length > 0 || parsedAutoSaves.invalidSaves.length > 0) {
+                corruptedSaves[cmdrUuid] = {
+                    manual: parsedManualSaves.invalidSaves.map((save) => {
+                        console.error("Corrupted manual save:", save.save, save.error);
+                        return save.save;
+                    }),
+                    auto: parsedAutoSaves.invalidSaves.map((save) => {
+                        console.error("Corrupted auto save:", save.save, save.error);
+                        return save.save;
+                    }),
+                };
+            }
+        }
+
+        backend.writeBackup(JSON.stringify(corruptedSaves));
+
+        if (Object.keys(corruptedSaves).length > 0) {
+            console.warn("Some save files could not be validated! Check the console for more information.");
         }
 
         const savesMap = new Map<string, CmdrSaves>();
-        for (const [cmdrId, cmdrSaves] of Object.entries(saveResult.value)) {
+        for (const [cmdrId, cmdrSaves] of Object.entries(saves)) {
             if (cmdrSaves.manual.length === 0 && cmdrSaves.auto.length === 0) {
                 continue;
             }
@@ -121,7 +194,7 @@ export class SaveManager implements ISaveManager {
      */
     private save(): boolean {
         const savesJson = Object.fromEntries(this.saves);
-        return this.backend.write(savesJson);
+        return this.backend.write(JSON.stringify(savesJson));
     }
 
     public renameCmdr(cmdrUuid: string, newName: string): void {
