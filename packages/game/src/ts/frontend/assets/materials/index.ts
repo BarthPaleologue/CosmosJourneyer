2d870218e8a8f9346b8fe482bfefb461ea6d5c1a
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-11-11 12:51:25 +0100

    Add glass material

--- packages/game/src/ts/frontend/assets/materials/index.ts
+++ packages/game/src/ts/frontend/assets/materials/index.ts
@@ -16,6 +16,7 @@
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import type { Material } from "@babylonjs/core/Materials/material";
+import { PBRMaterial } from "@babylonjs/core/Materials/PBR/pbrMaterial";
 import { PBRMetallicRoughnessMaterial } from "@babylonjs/core/Materials/PBR/pbrMetallicRoughnessMaterial";
 import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
 import { Color3 } from "@babylonjs/core/Maths/math.color";
@@ -37,6 +38,7 @@ export type Materials = {
     readonly tree: PBRMetallicRoughnessMaterial;
     readonly tank: PBRMetallicRoughnessMaterial;
     readonly tire: TireMaterial;
+    readonly glass: PBRMaterial;
 };
 
 export function initMaterials(textures: Textures, scene: Scene): Materials {
@@ -54,6 +56,16 @@ export function initMaterials(textures: Textures, scene: Scene): Materials {
 
     const tireMaterial = new TireMaterial(textures.materials.tire, scene);
 
+    const glass = new PBRMaterial("glass", scene);
+    glass.reflectivityColor = new Color3(0.2, 0.2, 0.2);
+    glass.albedoColor = new Color3(0.95, 0.95, 0.95);
+    glass.metallic = 0;
+    glass.roughness = 0.05;
+    glass.transparencyMode = PBRMaterial.PBRMATERIAL_ALPHABLEND;
+    glass.alpha = 0.1;
+    glass.indexOfRefraction = 1.5;
+    glass.backFaceCulling = false;
+
     return {
         butterfly: new ButterflyMaterial(textures.particles.butterfly, scene).get(),
         grass: new GrassMaterial(textures.noises.seamlessPerlin, scene).get(),
@@ -62,6 +74,7 @@ export function initMaterials(textures: Textures, scene: Scene): Materials {
         tree: treeMaterial,
         tank: tankMaterial,
         tire: tireMaterial,
+        glass: glass,
     };
 }
 
