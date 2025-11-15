d45d9ed0f5ec922c12f5b2b47c710c65dc44ecb7
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-11-15 14:34:10 +0100

    Use shadow util in character PG

--- packages/game/src/ts/frontend/controls/characterControls/characterControls.ts
+++ packages/game/src/ts/frontend/controls/characterControls/characterControls.ts
@@ -228,7 +228,6 @@ export class CharacterControls implements Controls {
         this.thirdPersonCamera.lowerRadiusLimit = 2;
         this.thirdPersonCamera.upperRadiusLimit = 500;
         this.thirdPersonCamera.minZ = 1;
-        this.thirdPersonCamera.maxZ = Settings.EARTH_RADIUS * 5;
         this.thirdPersonCamera.wheelPrecision *= 3;
         this.thirdPersonCamera.parent = this.getTransform();
 
