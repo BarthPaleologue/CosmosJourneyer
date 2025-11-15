1a4629610e59c7758bc987e68d79fca00d87573c
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-11-15 11:51:08 +0100

    Transfer overlay management to UI layer

--- packages/game/src/ts/frontend/ui/interactionLayer.ts
+++ packages/game/src/ts/frontend/ui/interactionLayer.ts
@@ -15,6 +15,9 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import { AbstractMesh } from "@babylonjs/core/Meshes/abstractMesh";
+import type { PhysicsBody } from "@babylonjs/core/Physics/v2/physicsBody";
+
 import { Settings } from "@/settings";
 
 import { pressInteractionToStrings } from "../helpers/inputControlsString";
@@ -28,6 +31,18 @@ export class InteractionLayer {
     private readonly crosshair: HTMLDivElement;
 
     private readonly interactionSystem: InteractionSystem;
+
+    private bodyToFadeIn: PhysicsBody | null = null;
+    private readonly bodiesToFadeOut = new Set<PhysicsBody>();
+
+    private readonly overlayColor = { r: 0.5, g: 0.5, b: 1 };
+    private readonly overlayAlpha = 0.2;
+
+    private readonly edgesColor = { r: 0.5, g: 0.5, b: 1 };
+    private readonly edgesAlpha = 1;
+
+    private readonly fadeOutSeconds = 0.1;
+    private readonly fadeInSeconds = 0.1;
 
     constructor(interactionSystem: InteractionSystem) {
         this.root = document.createElement("div");
@@ -64,7 +79,8 @@ export class InteractionLayer {
         this.interactionSystem = interactionSystem;
     }
 
-    update() {
+    public update(deltaSeconds: number) {
+        this.updateFadeAnimations(deltaSeconds);
         const currentInteractions = this.interactionSystem.getCurrentInteractions();
 
         if (
@@ -80,6 +96,69 @@ export class InteractionLayer {
             this.interactionText.style.display = "block";
             this.interactionText.innerText = `[${keyString}] ${currentInteractions[0].label}`;
             this.crosshair.style.display = "none";
+        }
+    }
+
+    private updateFadeAnimations(deltaSeconds: number) {
+        const currentTarget = this.interactionSystem.getCurrentTarget();
+        if (currentTarget === null && this.bodyToFadeIn !== null) {
+            this.bodiesToFadeOut.add(this.bodyToFadeIn);
+            this.bodyToFadeIn = null;
+        } else if (currentTarget !== null && this.bodyToFadeIn !== currentTarget) {
+            if (this.bodyToFadeIn !== null) {
+                this.bodiesToFadeOut.add(this.bodyToFadeIn);
+            }
+            this.bodyToFadeIn = currentTarget;
+            this.bodiesToFadeOut.delete(currentTarget);
+        }
+
+        this.updateFadeIn(deltaSeconds);
+        this.updateFadeOut(deltaSeconds);
+    }
+
+    private updateFadeIn(deltaSeconds: number) {
+        if (this.bodyToFadeIn === null) {
+            return;
+        }
+        const transform = this.bodyToFadeIn.transformNode;
+        if (!(transform instanceof AbstractMesh)) {
+            return;
+        }
+
+        if (!transform.renderOverlay) {
+            transform.renderOverlay = true;
+            transform.overlayColor.set(this.overlayColor.r, this.overlayColor.g, this.overlayColor.b);
+            transform.overlayAlpha = 0;
+        }
+
+        if (transform.edgesRenderer === null) {
+            transform.enableEdgesRendering();
+            transform.edgesColor.set(this.edgesColor.r, this.edgesColor.g, this.edgesColor.b, 0);
+        }
+
+        if (transform.overlayAlpha >= this.overlayAlpha && transform.edgesColor.a >= this.edgesAlpha) {
+            return;
+        }
+
+        transform.overlayAlpha += this.overlayAlpha * (deltaSeconds / this.fadeInSeconds);
+        transform.edgesColor.a += this.edgesAlpha * (deltaSeconds / this.fadeInSeconds);
+    }
+
+    private updateFadeOut(deltaSeconds: number) {
+        for (const body of this.bodiesToFadeOut) {
+            const transform = body.transformNode;
+            if (!(transform instanceof AbstractMesh)) {
+                this.bodiesToFadeOut.delete(body);
+                continue;
+            }
+
+            transform.overlayAlpha -= this.overlayAlpha * (deltaSeconds / this.fadeOutSeconds);
+            transform.edgesColor.a -= this.edgesAlpha * (deltaSeconds / this.fadeOutSeconds);
+            if (transform.overlayAlpha <= 0) {
+                transform.renderOverlay = false;
+                transform.disableEdgesRendering();
+                this.bodiesToFadeOut.delete(body);
+            }
         }
     }
 }
