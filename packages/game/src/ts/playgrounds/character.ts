49f1708a4b30ea37d3cdf0376d85ba290c9eb86e
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-12-04 22:07:43 +0100

    Populate charater pg

--- packages/game/src/ts/playgrounds/character.ts
+++ packages/game/src/ts/playgrounds/character.ts
@@ -16,6 +16,7 @@
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import {
+    Axis,
     Color3,
     DirectionalLight,
     HemisphericLight,
@@ -23,6 +24,7 @@ import {
     PBRMetallicRoughnessMaterial,
     PhysicsAggregate,
     PhysicsShapeType,
+    Space,
 } from "@babylonjs/core";
 import { type AbstractEngine } from "@babylonjs/core/Engines/abstractEngine";
 import { Vector3 } from "@babylonjs/core/Maths/math.vector";
@@ -32,6 +34,7 @@ import { type ILoadingProgressMonitor } from "@/frontend/assets/loadingProgressM
 import { loadHumanoidPrefabs } from "@/frontend/assets/objects/humanoids";
 import { CharacterControls } from "@/frontend/controls/characterControls/characterControls";
 import { CharacterInputs } from "@/frontend/controls/characterControls/characterControlsInputs";
+import { HumanoidAvatar } from "@/frontend/controls/characterControls/humanoidAvatar";
 
 import { createSky, enablePhysics, enableShadows } from "./utils";
 
@@ -48,7 +51,7 @@ export async function createCharacterDemoScene(
         await engine.getRenderingCanvas()?.requestPointerLock();
     });
 
-    const characters = await loadHumanoidPrefabs(scene, progressMonitor);
+    const humanoids = await loadHumanoidPrefabs(scene, progressMonitor);
 
     const light = new DirectionalLight("dir01", new Vector3(1, -2, -1), scene);
     light.position = new Vector3(5, 5, 5).scaleInPlace(10);
@@ -60,13 +63,28 @@ export async function createCharacterDemoScene(
 
     const groundRadius = 40;
 
-    const characterModel = characters.default.spawn();
-    if (!characterModel.success) {
-        throw new Error(`Failed to instantiate character: ${characterModel.error}`);
+    const humanoidInstance = humanoids.default.spawn();
+    if (!humanoidInstance.success) {
+        throw new Error(`Failed to instantiate character: ${humanoidInstance.error}`);
     }
 
-    const character = new CharacterControls(characterModel.value, scene);
+    const character = new CharacterControls(humanoidInstance.value, scene);
     character.getTransform().position.y = groundRadius;
+
+    const humanoid2 = humanoids.default.spawn();
+    if (!humanoid2.success) {
+        throw new Error(`Failed to instantiate character: ${humanoid2.error}`);
+    }
+    const character2 = new HumanoidAvatar(humanoid2.value, scene);
+    character2.getTransform().position = new Vector3(10, groundRadius, 6);
+
+    const humanoid3 = humanoids.default.spawn();
+    if (!humanoid3.success) {
+        throw new Error(`Failed to instantiate character: ${humanoid3.error}`);
+    }
+    const character3 = new HumanoidAvatar(humanoid3.value, scene);
+    character3.getTransform().position = new Vector3(10, groundRadius, 7.5);
+    character3.getTransform().rotate(Axis.Y, Math.PI, Space.WORLD);
 
     enableShadows(light);
 
@@ -87,9 +105,11 @@ export async function createCharacterDemoScene(
     groundMaterial.baseColor = new Color3(0.5, 0.5, 0.5);
     ground.material = groundMaterial;
 
-    character.setClosestWalkableObject({
+    const walkableObject = {
         getTransform: () => ground,
-    });
+    };
+
+    character.setClosestWalkableObject(walkableObject);
 
     scene.onBeforeRenderObservable.add(() => {
         if (character.getActiveCamera() !== scene.activeCamera) {
@@ -102,6 +122,8 @@ export async function createCharacterDemoScene(
 
         const deltaSeconds = engine.getDeltaTime() / 1000;
         character.update(deltaSeconds);
+        character2.update(deltaSeconds, walkableObject);
+        character3.update(deltaSeconds, walkableObject);
     });
 
     return scene;
