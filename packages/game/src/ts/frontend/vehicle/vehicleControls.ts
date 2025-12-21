dae53a0eb5c159eb45d1a149a086f713a2511236
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2025-12-21 18:00:34 +0100

    Basic interaction wiring for on foot PG

--- packages/game/src/ts/frontend/vehicle/vehicleControls.ts
+++ packages/game/src/ts/frontend/vehicle/vehicleControls.ts
@@ -36,9 +36,9 @@ export class VehicleControls implements Controls {
 
     private readonly thirdPersonCameraYOffset = 2;
 
-    private readonly thirdPersonCamera: ArcRotateCamera;
+    readonly thirdPersonCamera: ArcRotateCamera;
 
-    private readonly firstPersonCamera: FreeCamera;
+    readonly firstPersonCamera: FreeCamera;
 
     private activeCamera: Camera;
 
