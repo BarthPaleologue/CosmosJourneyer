8d67aa2637152c3e381fa64c17da1ef5953c6fd6
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2026-02-09 19:00:18 +0100

    Add model generation boilerplate for orbital facility sections

---
+++ packages/game/src/ts/backend/universe/proceduralGenerators/orbitalFacilities/sections/habitats/helix.ts
@@ -0,0 +1,28 @@
+//  This file is part of Cosmos Journeyer
+//
+//  Copyright (C) 2024 Barthélemy Paléologue <barth.paleologue@cosmosjourneyer.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import type { HelixHabitatModel } from "@/backend/universe/orbitalObjects/orbitalFacilities/sections/habitats/helix";
+
+export function generateHelixHabitatModel(seed: number): HelixHabitatModel {
+    return {
+        type: "helixHabitat",
+        surface: {
+            housing: 0,
+            agriculture: 0,
+        },
+    };
+}
