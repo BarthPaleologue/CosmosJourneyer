7e9a85c235d544d116edaf0023e3cbb44460da38
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2026-02-11 18:40:33 +0100

    Raise playwright timeout

--- packages/game/playwright.config.ts
+++ packages/game/playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
     testDir: "tests/e2e",
+    timeout: 180_000,
     use: {
         baseURL: "http://localhost:8080",
         browserName: "chromium",
