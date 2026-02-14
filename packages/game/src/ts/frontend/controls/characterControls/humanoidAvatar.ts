550e27adea69a64f9cce94e7048c0929ea37403c
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2026-02-14 13:41:05 +0100

    Improve surface querying robustness

--- packages/game/src/ts/frontend/controls/characterControls/humanoidAvatar.ts
+++ packages/game/src/ts/frontend/controls/characterControls/humanoidAvatar.ts
@@ -96,11 +96,16 @@ export class HumanoidAvatar implements Transformable {
 
     private lastSurfaceInfo: SurfaceInfo | null = null;
 
-    private readonly raycastResult = new PhysicsRaycastResult();
-
     private readonly root: AbstractMesh;
 
     private readonly physicsEngine: PhysicsEngineV2;
+
+    private readonly downwardRaycastResult = new PhysicsRaycastResult();
+    private readonly upwardRaycastResult = new PhysicsRaycastResult();
+    private readonly raycastQuery = {
+        membership: CollisionMask.EVERYTHING,
+        collideWith: CollisionMask.EVERYTHING & ~CollisionMask.AVATARS,
+    };
 
     private readonly mass = 80;
     readonly aggregate: PhysicsAggregate;
@@ -236,32 +241,50 @@ export class HumanoidAvatar implements Transformable {
         }
     }
 
-    private querySurfaceBelow() {
-        const up = this.getTransform().up;
-        const rayStartOffset = 150;
-        const rayDepth = 100;
-        const start = this.getTransform().getAbsolutePosition().add(up.scale(rayStartOffset));
-        const end = this.getTransform().getAbsolutePosition().add(up.scale(-rayDepth));
-        this.physicsEngine.raycastToRef(start, end, this.raycastResult, {
-            membership: CollisionMask.EVERYTHING,
-            collideWith: CollisionMask.EVERYTHING & ~CollisionMask.AVATARS,
-        });
+    private querySurface(): SurfaceInfo | null {
+        this.getTransform().computeWorldMatrix(true);
 
-        if (!this.raycastResult.hasHit) {
+        const downwardRayLength = 150;
+        const upwardRayLength = 20;
+        const rayOffset = 1.0;
+
+        const up = this.getTransform().up;
+        const rayOrigin = this.getTransform().getAbsolutePosition().add(up.scale(rayOffset));
+        const downwardRayEnd = rayOrigin.add(up.scale(-downwardRayLength));
+        const upwardRayEnd = rayOrigin.add(up.scale(upwardRayLength));
+
+        this.physicsEngine.raycastToRef(rayOrigin, downwardRayEnd, this.downwardRaycastResult, this.raycastQuery);
+        this.physicsEngine.raycastToRef(rayOrigin, upwardRayEnd, this.upwardRaycastResult, this.raycastQuery);
+
+        if (this.upwardRaycastResult.hasHit) {
+            if ((this.upwardRaycastResult.shape?.filterMembershipMask ?? 0) & CollisionMask.WATER) {
+                // water above head, we are swimming
+                const waterDistance = this.upwardRaycastResult.hitDistance - rayOffset;
+                return {
+                    type: "water" as const,
+                    distance: waterDistance,
+                };
+            } else if (!this.downwardRaycastResult.hasHit) {
+                // no ground below but ground above, could be falling under the ground
+                const groundDistance = -this.upwardRaycastResult.hitDistance + rayOffset;
+                return {
+                    type: "ground" as const,
+                    distance: groundDistance,
+                };
+            }
+
+            // ground above head and ground below, nothing to do here
+        }
+
+        if (!this.downwardRaycastResult.hasHit) {
+            // nothing above, nothing below, we are in the air
             return null;
         }
 
-        const distance = this.raycastResult.hitDistance - rayStartOffset;
-        if ((this.raycastResult.body?.shape?.filterMembershipMask ?? 0) & CollisionMask.WATER) {
-            return {
-                type: "water" as const,
-                distance,
-            };
-        }
-
+        const groundDistance = this.downwardRaycastResult.hitDistance - rayOffset;
         return {
             type: "ground" as const,
-            distance,
+            distance: groundDistance,
         };
     }
 
@@ -309,7 +332,7 @@ export class HumanoidAvatar implements Transformable {
     }
 
     public update(deltaSeconds: number): void {
-        this.lastSurfaceInfo = this.querySurfaceBelow();
+        this.lastSurfaceInfo = this.querySurface();
 
         // Do not fall through the ground
         if (
