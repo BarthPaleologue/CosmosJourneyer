1c78f63abc374746864d5ecd98947ab179b41eb2
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2026-07-13 18:10:18 +0200

    Remove legacy website implementation

--- packages/website/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { SocialLinks } from "./SocialLinks";
-export { ScrollArrow } from "./ScrollArrow";
-export { FAQ } from "./FAQ";
-export { RoadmapItemComponent as RoadmapItem } from "./RoadmapItem";
-export { RotatingCube } from "./RotatingCube";
