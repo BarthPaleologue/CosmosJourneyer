5928b391e33def5e6fc5a3da97b78f02f3b879a0
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2026-07-13 18:12:19 +0200

    Simplify website static export tooling

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -5,17 +5,6 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
-    // Remove experimental features that cause issues with static export
-    experimental: {},
-    compiler: {
-        removeConsole: process.env.NODE_ENV === "production",
-    },
-    // Enable source maps in development
-    productionBrowserSourceMaps: false,
-    // Compress static files
-    compress: true,
-    // Note: Headers don't work with static export mode
-    // If you need headers, deploy to a platform that supports them
 };
 
 module.exports = nextConfig;
